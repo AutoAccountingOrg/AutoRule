ecd5144236374947abab059567093b2cb50249c0
Ankio <ankio@ankio.net>
2023-12-20 21:34:42 +0800

    :sparkles: 支持支付宝收款码收款

--- src/rule/app/支付宝消息盒子/main.js
+++ src/rule/app/支付宝消息盒子/main.js
@@ -5,9 +5,33 @@ import {Currency} from "../../../utils/Currency";
 export function get(data) {
     data = JSON.parse(data)
     let pl = JSON.parse(data.pl)
-    if(pl.templateType === "BN"){
+    if(pl.templateType === "BN"){ //转账收款
         var dataItems = JSON.parse(pl.extraInfo)
-        return new RuleObject(BillType.Income,dataItems.money,dataItems.assistMsg1,dataItems.topSubContent,"","支付宝余额",0,Currency['人民币'],data.mct,"支付宝转账收款")
+        return new RuleObject(
+            BillType.Income,
+            dataItems.money,
+            dataItems.assistMsg1,
+            dataItems.topSubContent,
+            "",
+            "支付宝余额",
+            0,
+            Currency['人民币'],
+            data.mct,
+            "支付宝转账收款"
+        )
+    }else if(pl.templateType === "S"){ //收款码收款
+        var dataItems = JSON.parse(pl.extraInfo)
+        return new RuleObject(
+            BillType.Income,
+            dataItems.content.replace("收款金额￥",""),
+            dataItems.title,
+            dataItems.assistMsg2,
+            "",
+            "支付宝余额",
+            0,
+            Currency['人民币'],
+            data.mct,
+            "支付宝收款码收款")
     }
 
     return null;
