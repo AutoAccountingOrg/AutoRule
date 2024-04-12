60cf499f9c294804d0239bd6409f63c71306dad4
Peter Jones <zzy944541960@outlook.com>
2024-04-12 17:56:39 +0800

    :sparklse:新增适配收钱吧福利社消费通知 #40

---
+++ src/rule/app/com.tencent.mm/微信公众号收钱吧福利社/main.test.js
@@ -0,0 +1,24 @@
+const { get } = require('./main');
+const fs = require('fs');
+const path = require('path');
+
+test("收钱吧消费通知", () => {
+    const dataFilePath = path.join(__dirname, 'tests', '收钱吧消费通知.txt');
+    // 使用readFileSync来同步读取文件内容
+    const data = fs.readFileSync(dataFilePath, 'utf8')
+
+    let result = get(data);
+
+    expect(result).toEqual({
+        type: 0,
+        money: 2.00,
+        shopName: '三津汤包雅雀湖店',
+        shopItem: '门店收款',
+        accountNameFrom: '',
+        accountNameTo: '',
+        fee: 0,
+        currency: 'CNY',
+        time: '',
+        channel: '收钱吧福利社交易完成通知'
+    });
+});
