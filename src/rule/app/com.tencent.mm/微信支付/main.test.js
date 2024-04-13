96d404d10ea909b5b6edc187367f5f3cbeb8f961
Peter Jones <zzy944541960@outlook.com>
2024-04-14 01:12:59 +0800

    :sparkles:增加测试用例

--- src/rule/app/com.tencent.mm/微信支付/main.test.js
+++ src/rule/app/com.tencent.mm/微信支付/main.test.js
@@ -15,7 +15,7 @@ test("微信支付扫码付款（商家收款码）", () => testAnkio('微信支
         accountNameFrom: '零钱',
         accountNameTo: '',
         currency: 'CNY',
-        time: "",
+        time: 0,
         channel: '微信[微信支付]'
     }
 ]))
@@ -30,9 +30,73 @@ test("微信支付扫码付款（第三方收款码）", () => testAnkio('微信
         accountNameFrom: '零钱',
         accountNameTo: '',
         currency: 'CNY',
-        time: "",
+        time: 0,
+        channel: '微信[微信支付]'
+    },
+    {
+        type: 0,
+        money: 6.20,
+        fee: 0,
+        shopName: '',
+        shopItem: '',
+        accountNameFrom: '零钱通',
+        accountNameTo: '',
+        currency: 'CNY',
+        time: 0,
+        channel: '微信[微信支付]'
+    },
+    {
+        type: 0,
+        money: 12.70,
+        fee: 0,
+        shopName: '',
+        shopItem: '',
+        accountNameFrom: '零钱',
+        accountNameTo: '',
+        currency: 'CNY',
+        time: 0,
         channel: '微信[微信支付]'
     }
 ]))
 
+test("微信支付自动扣费", () => testAnkio('微信支付自动扣费',[
+    {
+        type: 0,
+        money: 53.72,
+        fee: 0,
+        shopName: '滴滴出行',
+        shopItem: '先乘后付',
+        accountNameFrom: '零钱通',
+        accountNameTo: '',
+        currency: 'CNY',
+        time: 0,
+        channel: '微信[微信支付]'
+    },
+    {
+        type: 0,
+        money: 51.70,
+        fee: 0,
+        shopName: '滴滴出行',
+        shopItem: '先乘后付',
+        accountNameFrom: '零钱通',
+        accountNameTo: '',
+        currency: 'CNY',
+        time: 0,
+        channel: '微信[微信支付]'
+    }
+]))
 
+test("微信支付收款", () => testAnkio('微信支付收款',[
+    {
+        type: 1,
+        money: 12.70,
+        fee: 0,
+        shopName: '',
+        shopItem: '今日第1笔收款，共计￥12.70',
+        accountNameFrom: '零钱',
+        accountNameTo: '',
+        currency: 'CNY',
+        time: 0,
+        channel: '微信[微信支付]'
+    }
+]))
