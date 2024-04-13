60733215fdd1e2a0ff319125aeda6aab3786c025
taigongzhaihua <944541960@qq.com>
2024-04-14 02:22:34 +0800

    :art: 优化代码

--- src/rule/app/com.tencent.mm/微信公众号收钱吧福利社/main.js
+++ src/rule/app/com.tencent.mm/微信公众号收钱吧福利社/main.js
@@ -2,17 +2,25 @@ import {BillType} from "../../../../utils/BillType";
 import {RuleObject} from "../../../../utils/RuleObject";
 import {Currency} from "../../../../utils/Currency";
 
+/**
+ * 处理数据并返回结果
+ * @param {string} data - 要处理的数据
+ * @returns {RuleObject|null} - 处理结果对象，如果处理失败则返回null
+ */
 export function get(data) {
     try {
-        data = JSON.parse(data);
-        const {description, source, title } = data.mMap;
+        // 解析数据
+        const {description, source, title } = JSON.parse(data).mMap;
 
+        // 检查源名称和标题是否匹配
         if (source !== "收钱吧福利社" || title !== "交易完成通知") {
             return null;
         }
 
-        const [_, money,_text1, shopName,shopItem] = description.match(/订单金额：(\d+(\.\d{2})?)元\n商品详情：(.*?) (.*?)?\n订单编号：\d+/);
+        // 使用解构赋值从description.match数组中提取值
+        const [_, money, , shopName, shopItem] = description.match(/订单金额：(\d+(\.\d{2})?)元\n商品详情：(.*?) (.*?)?\n订单编号：\d+/);
 
+        // 创建并返回RuleObject对象
         return new RuleObject(
             BillType.Expend,
             parseFloat(money),
@@ -26,6 +34,6 @@ export function get(data) {
             `微信[收钱吧消费通知]`
         );
     } catch (error) {
-        throw new error(`[微信公众号收钱吧福利社] get function: ${error}`);
+        throw new Error(`[微信公众号收钱吧福利社] get function: ${error}`);
     }
 }
