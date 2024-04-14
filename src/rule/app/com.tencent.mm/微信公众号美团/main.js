3443d0f26c49ba56f91f3f6d668dbfdf303be689
Ankio <ankio@ankio.net>
2024-04-14 08:33:22 +0800

    :sparkles: 适配美团消费 #44

---
+++ src/rule/app/com.tencent.mm/微信公众号美团/main.js
@@ -0,0 +1,72 @@
+import { RuleObject } from "../../../../utils/RuleObject";
+import { BillType } from "../../../../utils/BillType";
+import { Currency } from "../../../../utils/Currency";
+
+// 定义源名称和需要匹配的标题数组
+const SOURCE_NAME = "美团";
+const TITLES = ["支付成功通知"];
+
+// 定义用于解析文本的正则表达式
+const regex = /消费账户：(.*?)\n支付金额：(.*?)元\n支付方式：(.*?)\n支付时间：(.+)/;
+
+/**
+ * 解析文本并返回解析结果。
+ * @param {string} text - 需要解析的文本。
+ * @returns {Object|null} - 解析后的结果对象，如果解析失败则返回null。
+ */
+function parseText(text) {
+    let match = text.match(regex);
+    if (!match) return null;
+
+    // 解析数据
+    let type = BillType.Expend;
+    let time = match[4].trim();
+    let shopName = SOURCE_NAME;
+    let shopItem = match[1];
+    let money = parseFloat(match[2]);
+    let accountNameFrom = match[3];
+    let channel = "微信[美团消费]";
+
+    // 返回解析结果
+    return {
+        type,
+        time,
+        shopName,
+        shopItem,
+        money,
+        accountNameFrom,
+        channel
+    };
+}
+
+/**
+ * @param {string} data - 包含数据的JSON字符串。
+ * @returns {RuleObject|null} - 解析后的RuleObject对象，如果解析失败则返回null。
+ */
+export function get(data) {
+    // 解析数据
+    data = JSON.parse(data);
+    let mapItem = data.mMap;
+
+    // 检查源名称和标题是否匹配
+    if (mapItem.source !== SOURCE_NAME || !TITLES.includes(mapItem.title)) return null;
+
+    // 解析文本
+    let parsedData = parseText(mapItem.description);
+    // 检查解析结果是否有效
+    if (!parsedData || parsedData.type===null) return null;
+
+    // 创建并返回RuleObject对象
+    return new RuleObject(
+        parsedData.type,
+        parsedData.money,
+        parsedData.shopName,
+        parsedData.shopItem,
+        parsedData.accountNameFrom,
+        "",
+        0,
+        Currency['人民币'],
+        parsedData.time,
+        parsedData.channel
+    );
+}
