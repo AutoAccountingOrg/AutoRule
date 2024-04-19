88f09d83036e260b214c1b3f429255b9131117dc
Ankio <ankio@ankio.net>
2024-04-19 14:52:41 +0800

    :ambulance: 错误导入

--- src/rule/app/com.eg.android.AlipayGphone/支付宝红包/main.js
+++ src/rule/app/com.eg.android.AlipayGphone/支付宝红包/main.js
@@ -1,7 +1,7 @@
-import { BillType } from "../../../../utils/BillType";
-import { RuleObject } from "../../../../utils/RuleObject";
-import { Currency } from "../../../../utils/Currency";
-import { stripHtml } from "../../../../utils/Html";
+import { BillType } from '../../../../utils/BillType';
+import { RuleObject } from '../../../../utils/RuleObject';
+import { Currency } from '../../../../utils/Currency';
+import { stripHtml } from '../../../../utils/Html';
 
 /**
  * 从给定的数据中提取支付宝红包的相关信息并返回规则对象。
@@ -17,14 +17,14 @@ export function get(data) {
   }
   return new RuleObject(
     BillType.Income,
-    parseFloat(dataItems.statusLine1Text.replace("元", "")),
+    parseFloat(dataItems.statusLine1Text.replace('元', '')),
     stripHtml(dataItems.subtitle),
     dataItems.title,
-    "支付宝余额",
-    "",
+    '支付宝余额',
+    '',
     0,
-    Currency["人民币"],
+    Currency['人民币'],
     data[0].mct,
-    "支付宝[收红包]",
+    '支付宝[收红包]',
   );
 }
