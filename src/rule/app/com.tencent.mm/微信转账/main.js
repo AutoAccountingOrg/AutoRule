3ec94d156a2adccf7c105d16a8f226ee935bc518
Ankio <ankio@ankio.net>
2024-04-29 23:28:56 +0800

    :sparkles: 适配微信转账收款 #105

---
+++ src/rule/app/com.tencent.mm/微信转账/main.js
@@ -0,0 +1,28 @@
+import { RuleObject } from '../../../../utils/RuleObject';
+import { BillType } from '../../../../utils/BillType';
+import { Currency } from '../../../../utils/Currency';
+import { formatDate } from '../../../../utils/Time';
+import { DataType } from '../../../../utils/DataType';
+
+/**
+ * @param {string} data - 包含数据的JSON字符串。
+ * @returns {RuleObject|null} - 解析后的RuleObject对象，如果解析失败则返回null。
+ */
+export function get(data) {
+  // 解析数据
+  data = JSON.parse(data);
+
+  // 创建并返回RuleObject对象
+  return new RuleObject(
+    BillType.Income,
+    data.fee,
+    data.hookUser,
+    data.status_desc,
+    '零钱',
+    '',
+    0,
+    Currency['人民币'],
+    formatDate(data.desc_item_list[1].value, 'Y年M月D日 h:i:s'),
+    '微信[转账收款]',
+  );
+}
---
+++ src/rule/app/com.tencent.mm/微信转账/main.test.js
@@ -0,0 +1,21 @@
+const { get } = require('./main');
+const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
+const { DataType } = require('../../../../utils/DataType');
+const { formatDate } = require('../../../../utils/Time');
+
+testAnkioInit(get, __dirname, DataType.App, 'com.tencent.mm');
+test('微信转账收款', () =>
+  testAnkio('微信转账收款', [
+    {
+      type: 1,
+      money: 1700,
+      fee: 0,
+      shopName: '师大123',
+      shopItem: '你已收款，资金已存入零钱',
+      accountNameFrom: '零钱',
+      accountNameTo: '',
+      currency: 'CNY',
+      time: formatDate('2024年04月23日 11:50:05', 'Y年M月D日 h:i:s'),
+      channel: '微信[转账收款]',
+    },
+  ]));
