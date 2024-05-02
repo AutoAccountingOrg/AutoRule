4600be1c6085f34240fa64e4842e04fc55df56be
Ankio <ankio@ankio.net>
2024-05-02 20:29:30 +0800

    :sparkles: 适配平安银行信用卡 #108

--- src/utils/Number.js
+++ src/utils/Number.js
@@ -1,10 +1,21 @@
+function convertToNumber(value) {
+  if (typeof value === 'number') {
+    return value; // 如果已经是数字，则直接返回
+  } else if (typeof value === 'string') {
+    // 如果是字符串，则尝试将其转换为数字
+    const num = parseFloat(value);
+    return isNaN(num) ? 0 : num; // 返回转换后的数字，如果无法转换则返回 undefined
+  } else {
+    return 0; // 其他类型返回 undefined
+  }
+}
 /**
  * 金额格式化
  * @param number
  * @returns {number}
  */
 export function toFloat(number) {
-  return +parseFloat(number.toPrecision(2));
+  return +parseFloat(convertToNumber(number).toPrecision(4));
 }
 
 /**
@@ -13,5 +24,5 @@ export function toFloat(number) {
  * @returns {number}
  */
 export function toDoubleFloat(number) {
-  return toFloat(+parseFloat(number) / 100);
+  return toFloat(convertToNumber(number) / 100);
 }
