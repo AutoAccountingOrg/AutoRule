01170cde9d600fac9b23e829789aecd3475c74d1
Ankio <ankio@ankio.net>
2024-05-25 20:41:44 +0800

    :sparkles: 适配美团退款 #135

--- src/rule/app/com.tencent.mm/微信公众号美团/main.js
+++ src/rule/app/com.tencent.mm/微信公众号美团/main.js
@@ -2,14 +2,51 @@ import { RuleObject } from '../../../../utils/RuleObject';
 import { BillType } from '../../../../utils/BillType';
 import { Currency } from '../../../../utils/Currency';
 import { formatDate } from '../../../../utils/Time';
+import { toFloat } from '../../../../utils/Number';
 
 // 定义源名称和需要匹配的标题数组
 const SOURCE_NAME = '美团';
-const TITLES = ['支付成功通知'];
+const TITLES = ['支付成功通知', '退款通知'];
 
-// 定义用于解析文本的正则表达式
-const regex =
-  /消费账户：(.*?)\n支付金额：(人民币)?(.*?)元\n支付方式：(.*?)\n支付时间：(.+)/;
+const regexMap = new Map([
+  [
+    /消费账户：(.*?)\n支付金额：(人民币)?(.*?)元\n支付方式：(.*?)\n支付时间：(.+)/,
+    match => {
+      const [, shopItem, , money, accountNameFrom, time] = match;
+
+      // 返回解析结果
+      return {
+        type: BillType.Expend,
+        time: formatDate(
+          time.trim(),
+          time.indexOf('年') !== -1 ? 'Y年M月D日 h:i' : 'Y-M-D h:i:s',
+        ),
+        shopName: SOURCE_NAME,
+        shopItem: shopItem,
+        money: parseFloat(money),
+        accountNameFrom: accountNameFrom,
+        channel: `微信[${SOURCE_NAME}-消费]`,
+      };
+    },
+  ],
+  [
+    /退款原因：(.*?)，退回【(.*?)】\n退款金额：(\d+\.\d{2})元（实际退款金额）/,
+    match => {
+      const [, shopItem, accountNameFrom, money] = match;
+
+      // 返回解析结果
+      return {
+        type: BillType.Income,
+        shopName: SOURCE_NAME,
+        shopItem: shopItem,
+        money: toFloat(money),
+        accountNameFrom: accountNameFrom,
+        channel: `微信[${SOURCE_NAME}-退款]`,
+        time: formatDate(),
+      };
+    },
+  ],
+]);
 
 /**
  * 解析文本并返回解析结果。
@@ -17,25 +54,13 @@ const regex =
  * @returns {Object|null} - 解析后的结果对象，如果解析失败则返回null。
  */
 function parseText(text) {
-  let match = text.match(regex);
-  if (!match) return null;
-
-  // 解析数据
-  const [, shopItem, , money, accountNameFrom, time] = match;
-
-  // 返回解析结果
-  return {
-    type: BillType.Expend,
-    time: formatDate(
-      time.trim(),
-      time.indexOf('年') !== -1 ? 'Y年M月D日 h:i' : 'Y-M-D h:i:s',
-    ),
-    shopName: SOURCE_NAME,
-    shopItem: shopItem,
-    money: parseFloat(money),
-    accountNameFrom: accountNameFrom,
-    channel: '微信[美团消费]',
-  };
+  for (let [regex, handler] of regexMap) {
+    const match = text.match(regex);
+    if (match) {
+      return handler(match);
+    }
+  }
+  return null;
 }
 
 /**
--- src/rule/app/com.tencent.mm/微信公众号美团/main.test.js
+++ src/rule/app/com.tencent.mm/微信公众号美团/main.test.js
@@ -16,7 +16,7 @@ test('美团消费', () =>
       accountNameTo: '',
       currency: 'CNY',
       time: formatDate('2024年04月11日 18:05', 'Y年M月D日 h:i'),
-      channel: '微信[美团消费]',
+      channel: '微信[美团-消费]',
     },
     {
       type: 0,
@@ -28,7 +28,7 @@ test('美团消费', () =>
       accountNameTo: '',
       currency: 'CNY',
       time: formatDate('2024年04月12日 10:53', 'Y年M月D日 h:i'),
-      channel: '微信[美团消费]',
+      channel: '微信[美团-消费]',
     },
     {
       type: 0,
@@ -40,6 +40,21 @@ test('美团消费', () =>
       accountNameTo: '',
       currency: 'CNY',
       time: formatDate('2024-04-12 15:29:46', 'Y-M-D h:i:s'),
-      channel: '微信[美团消费]',
+      channel: '微信[美团-消费]',
+    },
+  ]));
+test('美团退款', () =>
+  testAnkio('美团退款', [
+    {
+      type: 1,
+      money: 52,
+      fee: 0,
+      shopName: '美团',
+      shopItem: '订单退款',
+      accountNameFrom: '招商银行信用卡(0898)',
+      accountNameTo: '',
+      currency: 'CNY',
+      time: formatDate(),
+      channel: '微信[美团-退款]',
     },
   ]));
