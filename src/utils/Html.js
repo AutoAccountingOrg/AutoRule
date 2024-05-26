78ac1d315182845b8c0228c9ff0abea9aa18d174
AnkioTomas <ankio@ankio.net>
2024-05-26 10:57:37 +0800

    :sparkles: 微信扫码付款 #170

--- src/utils/Html.js
+++ src/utils/Html.js
@@ -1,3 +1,12 @@
 export function stripHtml(html) {
   return html.replace(/<[^>]*>/g, '');
 }
+export function findNonEmptyString() {
+  let args = Array.from(arguments);
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] !== '' && typeof args[i] === 'string') {
+      return args[i];
+    }
+  }
+  return '';
+}
