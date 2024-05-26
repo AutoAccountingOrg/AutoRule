bf7d75d3dab14ad2524fb43d56f59122522b9310
AnkioTomas <ankio@ankio.net>
2024-05-26 10:28:58 +0800

    :sparkles: 支付宝消费奖励 #144

--- src/rule/app/com.eg.android.AlipayGphone/支付宝消息盒子/main.test.js
+++ src/rule/app/com.eg.android.AlipayGphone/支付宝消息盒子/main.test.js
@@ -159,6 +159,18 @@ test('支付宝消费奖励', () =>
       time: 1715479904000,
       channel: '支付宝[消费奖励]',
     },
+    {
+      type: 1,
+      money: 0.22,
+      fee: 0,
+      shopName: '吱信（上海）网络技术有限公司',
+      shopItem: '支付宝发红包，你赚现金奖励',
+      accountNameFrom: '支付宝余额',
+      accountNameTo: '',
+      currency: 'CNY',
+      time: 1715505776000,
+      channel: '支付宝[消费奖励]',
+    },
   ]));
 test('支付宝退款', () =>
   testAnkio('支付宝退款', [
