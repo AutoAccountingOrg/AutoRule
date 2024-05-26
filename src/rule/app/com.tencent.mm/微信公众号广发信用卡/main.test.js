25fd8b8c0d18f79c6987eda3e2c6a9089c24ca25
Ankio <ankio@ankio.net>
2024-05-26 20:52:52 +0800

    :sparkles: 广发信用卡支出 #173

---
+++ src/rule/app/com.tencent.mm/微信公众号广发信用卡/main.test.js
@@ -0,0 +1,24 @@
+const { get } = require('./main');
+const fs = require('fs');
+const path = require('path');
+const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
+const { DataType } = require('../../../../utils/DataType');
+const { formatDate } = require('../../../../utils/Time');
+
+testAnkioInit(get, __dirname, DataType.App, 'com.tencent.mm');
+
+test('广发信用卡支出', () =>
+  testAnkio('广发信用卡支出', [
+    {
+      type: 0,
+      money: 1275.68,
+      fee: 0,
+      shopName: '（特约）京东支付',
+      shopItem: '网银在线（北京）科技有',
+      accountNameFrom: '广发信用卡(4897)',
+      accountNameTo: '',
+      currency: 'CNY',
+      time: formatDate('05月26日20:01', 'M月D日h:i'),
+      channel: '微信[广发信用卡-支出]',
+    },
+  ]));
