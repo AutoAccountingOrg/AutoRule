e4488040f813a418f1cc94b6ccbe100a40c9b9ef
Ankio <ankio@ankio.net>
2024-05-27 21:33:47 +0800

    :sparkles: 农业银行 #179

--- src/rule/app/com.tencent.mm/微信公众号农业银行信用卡/main.js
+++ src/rule/app/com.tencent.mm/微信公众号农业银行信用卡/main.js
@@ -11,15 +11,26 @@ const TITLES_BOC = ['交易成功通知'];
 // 正则表达式和处理函数的映射关系
 const regexMapBOC = new Map([
   [
-    /交易时间：(.*?)\n交易类型：卡号尾号（(\d+)），网上支付\n交易金额：([\d,]+.\d{2})元\n可用余额：.*?元\n交易地址：(.*?)-(.*?)$/,
+    //交易时间：2024-05-27 00:30:49\n交易类型：卡号尾号（5981），退货\n交易金额：1281.56元\n可用余额：67591.21元\n交易地址：财付通退款
+    /交易时间：(.*?)\n交易类型：卡号尾号（(\d+)），(.*?)\n交易金额：([\d,]+.\d{2})元\n可用余额：.*?元\n交易地址：(.*?)$/,
     match => {
-      const [, time, number, money, shopName, shopItem] = match;
+      const [, time, number, type, money, shopName] = match;
+
+      let billType = BillType.Expend;
+      let channel = ``;
+      if (type.includes('退货')) {
+        billType = BillType.Income;
+        channel = `收入`;
+      } else if (type.includes('支付')) {
+        billType = BillType.Expend;
+        channel = `支出`;
+      }
 
       return {
         money: toFloat(money),
-        type: BillType.Expend,
+        type: billType,
         time: formatDate(time, 'Y-M-D h:i:s'), //2024-05-12 17:51
-        shopItem: shopItem,
+        shopItem: type,
         shopName: shopName,
         accountNameFrom: `${SOURCE_NAME_BOC}(${number})`,
         Currency: Currency['人民币'],
