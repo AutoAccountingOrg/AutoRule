d141c70b13a329208a7f03ccc8e4c709bd1a998b
ankio <ankio@ankio.net>
2024-09-02 23:01:20 +0800

    :package: dist: 更新依赖包
    更新了依赖包，以改进性能和稳定性。

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+export default {
+  "presets":[
+    "@babel/preset-env",
+  ],
+  "plugins":[]
+}
--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,12 @@ import { createFilter } from '@rollup/pluginutils';
 import { terser } from 'rollup-terser';
 import babel from '@rollup/plugin-babel';
 
+let babelConfig = {
+  "babelHelpers": 'bundled',  // 保持这个配置
+  "exclude": 'node_modules/**',
+};
 // 递归地获取所有规则文件
-function getRuleFiles(dirPath) {
+function getRuleFiles (dirPath) {
   let files = [];
 
   fs.readdirSync(dirPath).forEach(file => {
@@ -21,11 +25,12 @@ function getRuleFiles(dirPath) {
 
   return files;
 }
+
 const rulesFolder = path.join('src', 'rule');
 const utilsFolder = path.join('src', 'utils');
 const ruleFiles = getRuleFiles(rulesFolder);
 const externalFilter = createFilter([], [], {
-  "resolve": false,
+  'resolve': false
 });
 const rules = [];
 const outputs = ruleFiles.map(file => {
@@ -39,40 +44,31 @@ const outputs = ruleFiles.map(file => {
     ruleChineseName,
     ruleApp,
     ruleType,
-    "path": out,
+    'path': out
   });
   return {
-    "input": file,
-    "output": {
-      "file": path.join('dist', out), // 使用父文件夹名称作为输出文件名
-      "format": 'iife',
-      "name": ruleName, // 使用父文件夹名称作为全局变量名
-      //  exports: 'none', // 禁止导出模块的方式
-      //  banner: `let ${ruleName} = {};`,
-      // footer: `let ${ruleName} = ${ruleName};`, // 将变量暴露到全局对象 window 上
+    'input': file,
+    'output': {
+      'file': path.join('dist', out), // 使用父文件夹名称作为输出文件名
+      'format': 'iife',
+      'name': ruleName // 使用父文件夹名称作为全局变量名
     },
-    "plugins": [
+    'plugins': [
+      babel(babelConfig),
       terser(),
-      // resolve('', ''), // 解析 node_modules 中的模块
-      // commonjs(), // 将 CommonJS 模块转换为 ES6 模块
-      babel({
-        "babelHelpers": 'bundled', // Babel 帮助程序类型
-        "exclude": 'node_modules/**', // 排除 node_modules 目录
-        "presets": [
-          '@babel/preset-env', // 使用 Babel 预设
-        ],
-      }),
+
     ],
-    "external": id => externalFilter(id),
+    'external': id => externalFilter(id)
   };
 });
 // 删除dist文件夹
 // 如果dist文件夹存在就删除文件夹
 try {
-  fs.rmSync(path.join('dist'), { "recursive": true });
-} catch (e) {}
+  fs.rmSync(path.join('dist'), { 'recursive': true });
+} catch (e) {
+}
 // 创建dist文件夹
-fs.mkdirSync(path.join('dist'), { "recursive": true });
+fs.mkdirSync(path.join('dist'), { 'recursive': true });
 // 将规则信息写入rules.json文件
 fs.writeFileSync(
   path.join('dist', 'rules.json'),
@@ -80,55 +76,51 @@ fs.writeFileSync(
 );
 let moduleName = 'common';
 outputs.push({
-  "input": path.join('src', 'utils', 'index.js'),
-  "output": {
-    "file": path.join('dist', `${moduleName}.js`), // 输出文件路径
-    "format": 'iife', // 输出格式为 IIFE
-    "name": moduleName, // 全局变量名
-    "footer": `
-          let BillType = ${moduleName}.BillType;
-          let Currency = ${moduleName}.Currency;
-          let DataType = ${moduleName}.DataType;
-          let RuleObject = ${moduleName}.RuleObject;
-          let Html = {
+  'input': path.join('src', 'utils', 'index.js'),
+  'output': {
+    'file': path.join('dist', `${moduleName}.js`), // 输出文件路径
+    'format': 'iife', // 输出格式为 IIFE
+    'name': moduleName, // 全局变量名
+    'footer': `
+          var BillType = ${moduleName}.BillType;
+          var Currency = ${moduleName}.Currency;
+          var DataType = ${moduleName}.DataType;
+          var RuleObject = ${moduleName}.RuleObject;
+          var Html = {
           findNonEmptyString: ${moduleName}.findNonEmptyString,
           stripHtml: ${moduleName}.stripHtml,
           };
-          let Number = {
+          var Number = {
           toDoubleFloat: ${moduleName}.toDoubleFloat,
           toFloat: ${moduleName}.toFloat,
           };
-          let Time ={
+          var Time ={
           formatDate: ${moduleName}.formatDate,
           isTimeInRange: ${moduleName}.isTimeInRange,
           };
-      `,
+      `
   },
-  "plugins": [
-    terser(),
-    babel({
-      "babelHelpers": 'bundled', // Babel 帮助程序类型
-      "exclude": 'node_modules/**', // 排除 node_modules 目录
-      "presets": [
-        '@babel/preset-env', // 使用 Babel 预设
-      ],
-    }),
-  ],
+  'plugins': [
+    babel(babelConfig),
+    terser()
+  ]
 });
 
 outputs.push({
-  "input": path.join('src', 'category', 'main.js'),
-  "output": {
-    "file": path.join('dist', `category.js`), // 输出文件路径
-    "format": 'iife', // 输出格式为 IIFE
-    "name": 'category', // 全局变量名
+  'input': path.join('src', 'category', 'main.js'),
+  'output': {
+    'file': path.join('dist', `category.js`), // 输出文件路径
+    'format': 'iife', // 输出格式为 IIFE
+    'name': 'category' // 全局变量名
   },
-  "plugins": [terser()],
+  'plugins': [
+    babel(babelConfig),
+    terser()]
 });
 
-function putCategoryNames(){
+function putCategoryNames () {
   const file = path.join('src', 'category', 'main.js');
-  fs.readFile(file,function(error,data) {
+  fs.readFile(file, function(error, data) {
     const content = data.toString();
     const regex = /["']([\u4e00-\u9fa5]+)["']/g;
     let match;
@@ -137,21 +129,19 @@ function putCategoryNames(){
     while ((match = regex.exec(content)) !== null) {
       results.push(match[1]);  // match[1] 包含了捕获组中的中文字符串
     }
-    results = results.filter(item => item !== "默认账本");
+    results = results.filter(item => item !== '默认账本');
     //results去重
     results = [...new Set(results)];
-
 
     //写入文件
     fs.writeFileSync(
       path.join('dist', 'category.json'),
       JSON.stringify(results, null, 2)
     );
-  })
+  });
 
 }
 
 putCategoryNames();
-
 
 export default outputs;
