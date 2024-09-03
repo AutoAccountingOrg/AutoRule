8eb4ab13e558e07c3214a726064ad6db37533b0e
ankio <ankio@ankio.net>
2024-09-03 22:00:47 +0800

    :sparkles: (支付宝蚂蚁财富): 添加支付宝蚂蚁财富规则
    添加支付宝蚂蚁财富规则，可以识别并解析支付宝蚂蚁财富相关的交易信息。

---
+++ src/rule/com.eg.android.AlipayGphone/app/支付宝蚂蚁财富/main.js
@@ -0,0 +1,33 @@
+import { AliTools, BillType, Currency, RuleObject, stripHtml, toFloat } from 'common/index.js';
+
+
+
+function pay(pl,t){
+  let obj = new RuleObject(BillType.Income);
+  obj.money =  toFloat(pl.content);
+  if (obj.money < 0) {
+    obj.type = BillType.Expend;
+  }
+  obj.channel = `支付宝[蚂蚁财富]`;
+
+  let extras = JSON.parse(pl.extraInfo);
+
+  obj.time = t;
+
+  obj.shopItem = extras.assistMsg1;
+  obj.shopName = extras.title;
+  obj.accountNameFrom = `蚂蚁财富账户`;
+  return obj;
+}
+
+
+export function get(data) {
+  data = JSON.parse(data);
+  let pl = JSON.parse(data[0].pl);
+  let t = data[0].mct;
+  if (pl.title.indexOf('蚂蚁财富') !== -1 ) {
+    return pay(pl, t);
+  }
+
+  return null;
+}
---
+++ src/rule/com.eg.android.AlipayGphone/app/支付宝蚂蚁财富/main.test.js
@@ -0,0 +1,23 @@
+const { get } = require('./main');
+const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
+const { DataType } = require('../../../../utils/DataType');
+
+testAnkioInit(get, __dirname, DataType.App, 'com.eg.android.AlipayGphone');
+
+
+test('支付宝蚂蚁财富', () =>
+  testAnkio('支付宝蚂蚁财富', [
+    {
+      "type": 0,
+      "money": -401.03,
+      "fee": 0,
+      "shopName": '蚂蚁财富',
+      "shopItem": '2024-05-15总资产收益已更新',
+      "accountNameFrom": '蚂蚁财富账户',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": 1715843879000,
+      "channel": '支付宝[蚂蚁财富]',
+    },
+
+  ]));
