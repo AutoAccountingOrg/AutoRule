9664ac118101c5cf696e7708753b82a1e0d31b8a
ankio <ankio@ankio.net>
2024-09-17 15:30:53 +0800

    :sparkles: (api): 添加微信公众号微信收款助手规则 #196
    - 新增微信公众号微信收款助手规则，支持提现到账场景。
    - 规则解析提现金额、银行名称、交易时间等信息，并生成对应的账单数据。

---
+++ src/rule/com.tencent.mm/app/微信公众号微信收款助手/main.test.js
@@ -0,0 +1,20 @@
+const { get } = require('./main');
+const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
+const { formatDate } = require('common/index.js');
+
+testAnkioInit(get, __dirname, 'com.tencent.mm');
+test('提现到账', () =>
+  testAnkio('提现到账', [
+    {
+      "type": "Transfer",
+      "money": 7.0,
+      "fee": 0,
+      "shopName": '',
+      "shopItem": '',
+      "accountNameFrom": '微信经营账户',
+      "accountNameTo": '四川农信(1586)',
+      "currency": 'CNY',
+      "time": formatDate("2024-05-31 08:23:16","Y-M-D h:i:s"),
+      "channel": '微信[微信收款助手-提现]',
+    },
+  ]));
