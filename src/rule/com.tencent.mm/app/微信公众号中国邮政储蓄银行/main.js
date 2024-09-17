730394315c334e1ff36b2bfdf6e2e68f7fbe1f33
ankio <ankio@ankio.net>
2024-09-17 22:40:55 +0800

    :sparkles: (api): 添加中国邮政储蓄银行规则 #220
    - 添加中国邮政储蓄银行的收入规则。
    - 包含规则解析逻辑和测试用例。
    - 提高了规则的覆盖范围，更好地支持不同银行的交易记录解析。

---
+++ src/rule/com.tencent.mm/app/微信公众号中国邮政储蓄银行/main.js
@@ -0,0 +1,74 @@
+import { BillType, Currency, formatDate, RuleObject, toFloat } from 'common/index.js';
+
+// 定义源名称和需要匹配的标题数组
+const SOURCE_NAME_BOC = '中国邮政储蓄银行';
+const TITLES_BOC = ['交易提醒'];
+
+// 正则表达式和处理函数的映射关系
+const regexMapBOC = [
+  [
+    //交易时间：2024年06月14日23:19\n交易类型：(尾号7618)薪酬\n交易金额：(人民币)8683.33元
+    /交易时间：(.*?)\n交易类型：\(尾号(\d+)\)(.*?)\n交易金额：\(人民币\)([\d,]+.\d{2})元/,
+      match => {
+
+
+      return {
+        "money": toFloat(match[4]),
+        "type": BillType.Income,
+        "time": match[1],
+        "shopItem": match[3],
+        "accountNameFrom": `${SOURCE_NAME_BOC}(${match[2]})`,
+        "Currency": Currency['人民币'],
+        "channel": `微信[${SOURCE_NAME_BOC}-收入]`,
+      };
+    },
+  ],
+];
+
+/**
+ * @param {string} text - 需要解析的文本
+ * @returns {Object|null} - 解析结果对象，如果解析失败则返回null
+ */
+function parseBOCText(text) {
+  for (let [regex, handler] of regexMapBOC) {
+    const match = text.match(regex);
+    if (match) {
+      return handler(match);
+    }
+  }
+  return null;
+}
+
+/**
+ * @param {string} data - JSON格式的数据
+ * @returns {RuleObject|null} - 规则对象，如果获取失败则返回null
+ */
+export function get(data) {
+  const mapItem = JSON.parse(data).mMap;
+  if (
+    mapItem.source !== SOURCE_NAME_BOC ||
+    !TITLES_BOC.includes(mapItem.title)
+  ) {
+    return null;
+  }
+
+  // 解析文本
+  const parsedText = parseBOCText(mapItem.description);
+  if (!parsedText || parsedText.type === null) {
+    return null;
+  }
+
+  // 创建并返回RuleObject对象
+  return new RuleObject(
+    parsedText.type,
+    parsedText.money,
+    parsedText.shopName,
+    parsedText.shopItem,
+    parsedText.accountNameFrom,
+    parsedText.accountNameTo,
+    0,
+    parsedText.Currency, //024年06月14日23:19
+    formatDate(parsedText.time, 'Y年M月D日h:i'),
+    parsedText.channel
+  );
+}
---
+++ src/rule/com.tencent.mm/app/微信公众号中国邮政储蓄银行/main.test.js
@@ -0,0 +1,22 @@
+const { get } = require('./main');
+const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
+
+const { formatDate } = require('common/index.js');
+
+testAnkioInit(get, __dirname, 'com.tencent.mm');
+
+test('中国邮政储蓄银行收入', () =>
+  testAnkio('中国邮政储蓄银行收入', [
+    {
+      "type": "Income",
+      "money": 8683.33,
+      "fee": 0,
+      "shopName": '',
+      "shopItem": '薪酬',
+      "accountNameFrom": '中国邮政储蓄银行(7618)',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": formatDate('2024年06月14日23:19', 'Y年M月D日h:i'),
+      "channel": '微信[中国邮政储蓄银行-收入]',
+    },
+  ]));
