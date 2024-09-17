83b6c70e5103a8cc6d8ab36e6c52d9ec700eebb2
ankio <ankio@ankio.net>
2024-09-17 22:34:36 +0800

    :sparkles: (rule): 增加泉州农村商业银行微信公众号规则 #222
    新增泉州农村商业银行微信公众号规则，用于解析其消息内容，提取交易信息。

---
+++ src/rule/com.tencent.mm/app/微信公众号泉州农村商业银行/main.js
@@ -0,0 +1,72 @@
+import { BillType, Currency, formatDate, RuleObject, toFloat } from 'common/index.js';
+
+// 定义源名称和需要匹配的标题数组
+const SOURCE_NAME_BOC = '泉州农村商业银行';
+const TITLES_BOC = ['交易提醒'];
+
+// 正则表达式和处理函数的映射关系
+const regexMapBOC = [
+  [
+    //交易时间：2024年06月16日 12时36分\n交易类型：转出支出（尾号2467储蓄账户）\n交易金额：人民币0.01元（对方户名:WL财付通微信转账:微...\n卡内余额：人民币393.18元
+    /交易时间：(.*?)\n交易类型：转出支出（尾号(\d+)储蓄账户）\n交易金额：人民币([\d,]+.\d{2})元（对方户名:(.*?)\n卡内余额：人民币.*?元/,
+      match => {
+      return {
+        "money": toFloat(match[3]),
+        "type": BillType.Expend,
+        "time": match[1],
+        "shopItem": match[4],
+        "accountNameFrom": `${SOURCE_NAME_BOC}(${match[2]})`,
+        "Currency": Currency['人民币'],
+        "channel": `微信[${SOURCE_NAME_BOC}-消费]`,
+      };
+    },
+  ],
+];
+
+/**
+ * @param {string} text - 需要解析的文本
+ * @returns {Object|null} - 解析结果对象，如果解析失败则返回null
+ */
+function parseBOCText(text) {
+  for (let [regex, handler] of regexMapBOC) {
+    const match = text.match(regex);
+    if (match) {
+      return handler(match);
+    }
+  }
+  return null;
+}
+
+/**
+ * @param {string} data - JSON格式的数据
+ * @returns {RuleObject|null} - 规则对象，如果获取失败则返回null
+ */
+export function get(data) {
+  const mapItem = JSON.parse(data).mMap;
+  if (
+    mapItem.source !== SOURCE_NAME_BOC ||
+    !TITLES_BOC.includes(mapItem.title)
+  ) {
+    return null;
+  }
+
+  // 解析文本
+  const parsedText = parseBOCText(mapItem.description);
+  if (!parsedText || parsedText.type === null) {
+    return null;
+  }
+
+  // 创建并返回RuleObject对象
+  return new RuleObject(
+    parsedText.type,
+    parsedText.money,
+    parsedText.shopName,
+    parsedText.shopItem,
+    parsedText.accountNameFrom,
+    parsedText.accountNameTo,
+    0,
+    parsedText.Currency, //5月7日10:50
+    formatDate(parsedText.time, 'Y年M月D日 h时i分'),
+    parsedText.channel
+  );
+}
