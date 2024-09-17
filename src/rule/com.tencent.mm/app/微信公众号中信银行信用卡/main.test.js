eb7a2fc5c7ea59879b0a15b5ace71479e11bb5cc
ankio <ankio@ankio.net>
2024-09-17 12:57:27 +0800

    :construction: (ui): 更新用户界面
    更新用户界面以改善用户体验并修复一些错误。

--- src/rule/com.tencent.mm/app/微信公众号中信银行信用卡/main.test.js
+++ src/rule/com.tencent.mm/app/微信公众号中信银行信用卡/main.test.js
@@ -1,6 +1,4 @@
 const { get } = require('./main');
-const fs = require('fs');
-const path = require('path');
 const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
 
 const { formatDate } = require('common/index.js');
@@ -21,5 +19,4 @@ test('中信银行消费', () =>
       "time": formatDate('07月11日19:22', 'M月D日h:i'),
       "channel": '微信[中信银行信用卡-消费]',
     },
-
   ]));
