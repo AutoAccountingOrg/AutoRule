9d1b868dc1940bc86a953609fb15dadbcb59de01
ankio <ankio@ankio.net>
2024-09-19 09:21:50 +0800

    :bug: (支付宝网商银行): 修复网商银行支出解析错误
    修复了网商银行支出解析中，金额未正确转换的问题。
    同时，将测试用例添加至 `tests/支付宝网商银行支出.txt`，以保证未来修改不会造成同样的错误。

--- src/rule/com.eg.android.AlipayGphone/app/支付宝网商银行/main.js
+++ src/rule/com.eg.android.AlipayGphone/app/支付宝网商银行/main.js
@@ -3,7 +3,7 @@ import { BillType, RuleObject, toFloat } from 'common/index.js';
 function transfer(pl,t){
   let obj = new RuleObject(BillType.Transfer);
   obj.money =  toFloat(pl.content);
-  obj.channel = `支付宝[网商银行-支出]`;
+  obj.channel = `支付宝[网商银行-转账]`;
 
   let extras = JSON.parse(pl.extraInfo);
 
@@ -16,14 +16,36 @@ function transfer(pl,t){
   return obj;
 }
 
+function expand(pl,t){
+  let obj = new RuleObject(BillType.Expend);
+  obj.money =  toFloat(pl.content);
+  obj.channel = `支付宝[网商银行-支出]`;
+
+  let extras = JSON.parse(pl.extraInfo);
+
+  obj.time = t;
+
+  obj.shopItem = extras.assistMsg1;
+  obj.shopName = "网商银行";
+  obj.accountNameFrom = `${extras.title}(${extras.assistMsg2})`;
+  obj.accountNameTo = ``;
+  return obj;
+}
+
 
 export function get(data) {
   data = JSON.parse(data);
   let pl = JSON.parse(data[0].pl);
   let t = data[0].mct;
-  if (pl.title.indexOf('网商银行') !== -1 ) {
+  if (pl.title.indexOf('网商银行') === -1 ) {
+   return null
+  }
+
+  if (pl.templateName.indexOf("交易成功提醒")===-1){
     return transfer(pl, t);
   }
+
+  return expand(pl,t);
 
   return null;
 }
--- src/rule/com.eg.android.AlipayGphone/app/支付宝网商银行/main.test.js
+++ src/rule/com.eg.android.AlipayGphone/app/支付宝网商银行/main.test.js
@@ -17,6 +17,22 @@ test('支付宝网商银行转出', () =>
       "accountNameTo": '工商银行(8464)',
       "currency": 'CNY',
       "time": 1712916392000,
+      "channel": '支付宝[网商银行-转账]',
+    },
+
+  ]));
+test('支付宝网商银行支出', () =>
+  testAnkio('支付宝网商银行支出', [
+    {
+      "type": "Expend",
+      "money": 0.33,
+      "fee": 0,
+      "shopName": '网商银行',
+      "shopItem": '支付宝支付',
+      "accountNameFrom": '网商银行(9578)',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": 1726667615000,
       "channel": '支付宝[网商银行-支出]',
     },
 
