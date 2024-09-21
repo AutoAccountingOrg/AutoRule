5d72de76be67827f7389a06f6193f62baa13b254
ankio <ankio@ankio.net>
2024-09-21 19:25:03 +0800

    :sparkles: (rule): 添加美团月付支出规则 #297
    添加美团月付支出规则，用于识别并记录美团月付的支出。

---
+++ src/rule/com.sankuai.meituan/notice/美团月付支出/main.js
@@ -0,0 +1,29 @@
+import { AliTools, BillType, formatDate, RuleObject, toFloat } from 'common/index.js';
+
+
+// 您账户6598于09月19日08:46在【财付通-理财通-腾安基金销售（深圳）有限公司】发生快捷支付扣款，人民币2500.00
+
+
+export function get(data) {
+  let json = JSON.parse(data)
+  // 【美团月付】成功支付126.00元
+ // let regex  = /您尾号(\d{4})的招行一卡通入账人民币([\d,]+.\d{2})元/
+  let regex = /【美团月付】成功支付([\d,]+.\d{2})元/
+  const match = json.title.match(regex);
+  if (!match) {
+    return null;
+  }
+  let [,money] = match;
+  let obj = new RuleObject();
+
+  obj.money = toFloat(money);
+  obj.channel = `美团月付[支出]`;
+  obj.shopName ="美团";
+  obj.shopItem = json.text;
+  obj.time = formatDate();
+
+  obj.type = BillType.Expend
+
+  obj.accountNameFrom = `美团月付`;
+  return obj;
+}
---
+++ src/rule/com.sankuai.meituan/notice/美团月付支出/main.test.js
@@ -0,0 +1,21 @@
+const { get } = require('./main');
+const { testAnkio, testAnkioInit } = require('../../../../tests/TestUtils');
+const { formatDate } = require('common/Time.js');
+
+
+testAnkioInit(get, __dirname, 'com.sankuai.meituan');
+test('美团月付支出', () =>
+  testAnkio('美团月付支出', [
+    {
+      "type": "Expend",
+      "money": 126,
+      "fee": 0,
+      "shopName": '美团',
+      "shopItem": '还款日每月8号，记得按时还款，查看额度变化>>',
+      "accountNameFrom": '美团月付',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": formatDate(),
+      "channel": '美团月付[支出]',
+    },
+  ]));
