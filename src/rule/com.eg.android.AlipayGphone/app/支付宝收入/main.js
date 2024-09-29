0c1a98b58b1e8a0b30340125d05c2497926b7c75
ankio <ankio@ankio.net>
2024-09-29 16:38:25 +0800

    :card_file_box: (支付宝收入): 支付宝收入规则测试用例
    新增了支付宝收入的测试用例，并根据实际情况进行了调整。
    - 添加了支付宝收入的测试用例
    - 调整了测试用例的结构和内容，使其更符合实际情况

--- src/rule/com.eg.android.AlipayGphone/app/支付宝收入/main.js
+++ src/rule/com.eg.android.AlipayGphone/app/支付宝收入/main.js
@@ -1,9 +1,11 @@
 import { AliTools, BillType, RuleObject, toFloat } from 'common/index.js';
 
-function expend(pl,t){
-  let obj = new RuleObject(BillType.Expend);
 
-  obj.channel = `支付宝[消费-支出]`;
+
+function income(pl,t){
+  let obj = new RuleObject(BillType.Income);
+
+  obj.channel = `支付宝[消费-收入]`;
 
   let content = JSON.parse(pl.content);
 
@@ -11,17 +13,18 @@ function expend(pl,t){
   obj.time = t;
   obj.money =  toFloat(content.money);
   obj.shopItem = obj.shopItem || pl.homePageTitle;
+  obj.shopName = obj.shopName || pl.title;
+  obj.accountNameFrom = obj.accountNameFrom || '支付宝余额';
   return obj;
 }
-
 
 
 export function get(data) {
   data = JSON.parse(data);
   let pl = JSON.parse(data[0].pl);
   let t = data[0].mct;
-  if (pl.title.indexOf('付款成功') !== -1 || pl.title.indexOf('自动扣款') !== -1 || pl.title.indexOf('Payment successful') !== -1) {
-    return expend(pl, t);
+   if (pl.homePageTitle.indexOf('收到一笔奖励') !== -1 || pl.title.indexOf('资金到账通知') !== -1  || pl.title.indexOf('退款通知') !== -1) {
+    return income(pl, t);
   }
 
   return null;
---
+++ src/rule/com.eg.android.AlipayGphone/app/支付宝收入/main.test.js
@@ -0,0 +1,45 @@
+const { get } = require('./main');
+const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
+
+
+testAnkioInit(get, __dirname, 'com.eg.android.AlipayGphone');
+
+test('支付宝收入', () =>
+  testAnkio('支付宝收入', [
+    {
+      "type": "Income",
+      "money": 0.02,
+      "fee": 0,
+      "shopName": '吱信（上海）网络技术有限公司',
+      "shopItem": '支付宝发红包，你赚现金奖励',
+      "accountNameFrom": '支付宝余额',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": 1715479904000,
+      "channel": '支付宝[消费-收入]',
+    },
+    {
+      "type": "Income",
+      "money": 0.3,
+      "fee": 0,
+      "shopName": '北京快手科技有限公司 bjk***@kuaishou.com',
+      "shopItem": '收款到账￥0.30',
+      "accountNameFrom": '支付宝余额',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": 1714212854000,
+      "channel": '支付宝[消费-收入]',
+    },
+    {
+      "type": "Income",
+      "money": 29.82,
+      "fee": 0,
+      "shopName": '退款通知',
+      "shopItem": '退款-麻爪爪·酸辣凤爪·卤味小吃(大朗里悦里店)外卖订单',
+      "accountNameFrom": '农业银行储蓄卡(9979)',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": 1710669984000,
+      "channel": '支付宝[消费-收入]',
+    },
+  ]));
