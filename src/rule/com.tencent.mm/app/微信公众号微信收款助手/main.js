c98e81e96d92029e78b7d8d4664cd810ebb6b6fa
ankio <ankio@ankio.net>
2024-09-29 22:38:43 +0800

    :recycle: (rule): 重构微信公众号微信收款助手/微商银行规则解析
    优化微信公众号微信收款助手和微商银行的规则解析代码，使用统一的解析函数和格式化函数。
    
    - 使用 `parseWechat` 函数进行文本解析，避免重复代码。
    - 使用 `formatDate` 函数格式化日期，提高代码可读性。
    - 使用 `RuleObject` 类构建规则对象，统一数据结构。
    - 使用 `findNonEmptyString` 函数获取首个非空字符串，简化代码逻辑。
    - 使用 `transferCurrency` 函数将货币单位转换为统一格式，提高代码可读性。

--- src/rule/com.tencent.mm/app/微信公众号微信收款助手/main.js
+++ src/rule/com.tencent.mm/app/微信公众号微信收款助手/main.js
@@ -1,42 +1,34 @@
-import { BillType, Currency, formatDate, RuleObject, toFloat,findNonEmptyString } from 'common/index.js';
+import { BillType, formatDate, RuleObject, toFloat, findNonEmptyString, parseWechat } from 'common/index.js';
 
 // 定义源名称和需要匹配的标题数组
-const SOURCE_NAME_WECHAT = '微信收款助手';
-const TITLES_WECHAT = [
+const SOURCE = '微信收款助手';
+const TITLE = [
   '经营账户提现到账',
 ];
-var mapItem;
 
 // 正则表达式和处理函数的映射关系
-const regexMap =[
+const rules =[
   [
     // 提现金额：¥7.00\n提现银行：四川农信(1586)\n提现时间：2024-05-31 08:23:16\n提现到账时间：2024-05-31 08:23:27
     /提现金额：¥(\d+\.\d{2})\n提现银行：(.*?)\n提现时间：.*?\n提现到账时间：(.*?)$/,
-    match => ({
-      "money": toFloat(match[1]),
-      "accountNameFrom": "微信经营账户",
-      "accountNameTo": match[2],
-      "time": formatDate(match[3],"Y-M-D h:i:s"),
-      "type": BillType.Transfer,
-      "channel": `微信[${SOURCE_NAME_WECHAT}-提现]`,
-    }),
+    match => {
+      const [, money, accountNameFrom, time] = match;
+      return new RuleObject(
+        BillType.Transfer,
+        toFloat(money),
+        '',
+        '',
+        '微信经营账户',
+        accountNameFrom,
+        0.0,
+        'CNY',
+        formatDate(time, 'Y-M-D h:i:s'),
+        `微信[${SOURCE}-提现]`
+      );
+    },
   ],
 ];
 
-/**
- * 解析微信支付文本
- * @param {string} text - 需要解析的文本
- * @returns {Object|null} - 解析结果对象，如果解析失败则返回null
- */
-function parseWeChatText(text) {
-  for (let [regex, handler] of regexMap) {
-    const match = text.match(regex);
-    if (match) {
-      return handler(match);
-    }
-  }
-  return null;
-}
 
 /**
  * 获取规则对象
@@ -44,32 +36,5 @@ function parseWeChatText(text) {
  * @returns {RuleObject|null} - 规则对象，如果获取失败则返回null
  */
 export function get(data) {
-  mapItem = JSON.parse(data).mMap;
-  if (
-    mapItem.source !== SOURCE_NAME_WECHAT ||
-    !TITLES_WECHAT.includes(mapItem.title.replace(/\d+\.\d{2}/, ''))
-  ) {
-    return null;
-  }
-  // 解析文本
-  const parsedText = parseWeChatText(mapItem.description);
-  if (!parsedText || parsedText.type === null) {
-    return null;
-  }
-
-  // 创建并返回RuleObject对象
-  return new RuleObject(
-    parsedText.type,
-    parsedText.money,
-    findNonEmptyString(
-      parsedText.shopName,
-      mapItem.display_name
-    ),
-    parsedText.shopItem,
-    parsedText.accountNameFrom,
-    parsedText.accountNameTo,
-    0,
-    Currency['人民币'],
-    parsedText.time || formatDate(),
-    parsedText.channel  );
+  return parseWechat(data,rules,SOURCE,TITLE)
 }
--- src/rule/com.tencent.mm/app/微信公众号微商银行/main.js
+++ src/rule/com.tencent.mm/app/微信公众号微商银行/main.js
@@ -1,80 +1,54 @@
-import { BillType, Currency, formatDate, RuleObject } from 'common/index.js';
+import { BillType, Currency, formatDate, parseWechat, RuleObject, toFloat, transferCurrency } from 'common/index.js';
 
 // 定义源名称和需要匹配的标题数组
 const SOURCE = '徽商银行';
-const TITLES = ['交易提醒'];
+const TITLE = ['交易提醒'];
 
 // 正则表达式和处理函数的映射关系
-const regexMapBOC = [
+const rules = [
   [
     /交易时间：(.*?)\n交易类型：快捷支付-(.*?)\n交易金额：(.*?)\(尾号(\d+)(.*?)\)\n卡内余额：(.*?)/,
-    match => ({
-      "money": parseFloat(match[3].replace(',', '')),
-      "type": BillType.Expend,
-      "time": `${match[1]}`,
-      "shopItem": match[2],
-      "accountNameFrom": `${match[5]}(${match[4]})`,
-      "Currency": Currency['人民币'],
-      "channel": `微信[${SOURCE}-消费]`,
-    }),
+    match => {
+      const [, time, shopItem, money, number,card, balance] = match;
+      return new RuleObject(
+        BillType.Expend,
+        toFloat(money),
+        '',
+        shopItem,
+        `${card}(${number})`,
+        '',
+        0.0,
+        Currency['人民币'],
+        formatDate(time, 'M月D日 h:i:s'),
+        `微信[${SOURCE}-消费]`
+      );
+    }
   ],
   [
     /交易时间：(.*?)\n交易类型：退款-(.*?)\n交易金额：(.*?)\(尾号(\d+)(.*?)\)\n卡内余额：(.*?)/,
-    match => ({
-      "money": parseFloat(match[3].replace(',', '')),
-      "type": BillType.Income,
-      "time": `${match[1]}`,
-      "shopItem": match[2],
-      "accountNameFrom": `${match[5]}(${match[4]})`,
-      "Currency": Currency['人民币'],
-      "channel": `微信[${SOURCE}-退款]`,
-    }),
+    match => {
+      const [, time, shopItem, money, number, currency, balance] = match;
+      return new RuleObject(
+        BillType.Income,
+        toFloat(money),
+        '',
+        shopItem,
+        `${SOURCE}(${number})`,
+        '',
+        0.0,
+        Currency['人民币'],
+        formatDate(time, 'M月D日 h:i:s'),
+        `微信[${SOURCE}-退款]`
+      );
+    },
   ],
 ];
 
-/**
- * @param {string} text - 需要解析的文本
- * @returns {Object|null} - 解析结果对象，如果解析失败则返回null
- */
-function parseBOCText(text) {
-  for (let [regex, handler] of regexMapBOC) {
-    const match = text.match(regex);
-    if (match) {
-      return handler(match);
-    }
-  }
-  return null;
-}
 
 /**
  * @param {string} data - JSON格式的数据
  * @returns {RuleObject|null} - 规则对象，如果获取失败则返回null
  */
 export function get(data) {
-  const mapItem = JSON.parse(data).mMap;
-  if (
-    mapItem.source !== SOURCE ||
-    !TITLES.includes(mapItem.title)
-  ) {
-    return null;
-  }
-
-  // 解析文本
-  const parsedText = parseBOCText(mapItem.description);
-  if (!parsedText || parsedText.type === null) {
-    return null;
-  }
-
-  // 创建并返回RuleObject对象
-  return new RuleObject(
-    parsedText.type,
-    parsedText.money,
-    parsedText.shopName,
-    parsedText.shopItem,
-    parsedText.accountNameFrom,
-    parsedText.accountNameTo,
-    0,
-    parsedText.Currency,
-    formatDate(parsedText.time, 'M月D日 h:i:s'),
-    parsedText.channel  );
+  return parseWechat(data, rules, SOURCE, TITLE)
 }
