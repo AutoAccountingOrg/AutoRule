b524f5e21e065fe8a7029ab55465b2ddd0bfa196
ankio <ankio@ankio.net>
2024-09-29 20:30:05 +0800

    :card_file_box: (db): 优化微信公众号中国民生银行规则解析
    - 使用 parseWechat 函数统一处理规则匹配，简化代码逻辑。
    - 使用 RuleObject 类封装解析结果，提升代码可读性和可维护性。
    - 删除冗余代码，优化代码结构。

--- src/rule/com.tencent.mm/app/微信公众号中国民生银行/main.js
+++ src/rule/com.tencent.mm/app/微信公众号中国民生银行/main.js
@@ -1,17 +1,16 @@
-import { BillType, Currency, formatDate, RuleObject, toFloat } from 'common/index.js';
+import { BillType, Currency, formatDate, parseWechat, RuleObject, toFloat } from 'common/index.js';
 
 // 定义源名称和需要匹配的标题数组
 const SOURCE = '中国民生银行';
-const TITLES = ['交易提醒'];
+const TITLE = ['交易提醒'];
 
 // 正则表达式和处理函数的映射关系
-const regexMapBOC = [
+const rules = [
   [
     /交易账号:借记卡（(\d+)）\n交易时间:(.*?)\n交易类型:(.*?)-(.*?)\n交易金额:(.*?) ([\d]+.\d{2})\n交易对象:(.*?)$/,
     match => {
-      var matchType = match[3];
-      var matchTypeName = '';
-      var shopItem = match[4];
+      let [, number, time, matchType, shopItem, currency, money,shopName] = match;
+      let matchTypeName = '';
       switch (matchType) {
         case '存入':
           matchTypeName = '收入';
@@ -23,63 +22,27 @@ const regexMapBOC = [
           break;
       }
 
-      return {
-        "money": toFloat(match[6]),
-        "type": matchType,
-        "time": match[2],
-        "shopName": match[7],
-        "shopItem": shopItem,
-        "accountNameFrom": `${SOURCE}借记卡(${match[1]})`,
-        "Currency": Currency[match[5]],
-        "channel": `微信[${SOURCE}-${matchTypeName}]`,
-      };
+      return new RuleObject(
+        matchType,
+        toFloat(money),
+        shopName,
+        shopItem,
+        `${SOURCE}借记卡(${number})`,
+        '',
+        0.0,
+        Currency[currency],
+        formatDate(time, 'Y年M月D日 h:i:s'),
+        `微信[${SOURCE}-${matchTypeName}]`
+      )
     },
   ],
 ];
 
-/**
- * @param {string} text - 需要解析的文本
- * @returns {Object|null} - 解析结果对象，如果解析失败则返回null
- */
-function parseBOCText(text) {
-  for (let [regex, handler] of regexMapBOC) {
-    const match = text.match(regex);
-    if (match) {
-      return handler(match);
-    }
-  }
-  return null;
-}
 
 /**
  * @param {string} data - JSON格式的数据
  * @returns {RuleObject|null} - 规则对象，如果获取失败则返回null
  */
 export function get(data) {
-  const mapItem = JSON.parse(data).mMap;
-  if (
-    mapItem.source !== SOURCE ||
-    !TITLES.includes(mapItem.title)
-  ) {
-    return null;
-  }
-
-  // 解析文本
-  const parsedText = parseBOCText(mapItem.description);
-  if (!parsedText || parsedText.type === null) {
-    return null;
-  }
-
-  // 创建并返回RuleObject对象
-  return new RuleObject(
-    parsedText.type,
-    parsedText.money,
-    parsedText.shopName,
-    parsedText.shopItem,
-    parsedText.accountNameFrom,
-    parsedText.accountNameTo,
-    0,
-    parsedText.Currency, //2024年05月10日 09:34:47
-    formatDate(parsedText.time, 'Y年M月D日 h:i:s'),
-    parsedText.channel  );
+  return parseWechat(data, rules, SOURCE, TITLE);
 }
