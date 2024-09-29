a986adeae2121fdb7deff6c53b7113490e08aee2
ankio <ankio@ankio.net>
2024-09-29 20:39:12 +0800

    :recycle: (api): 重构微信公众号中国邮政储蓄银行规则
    优化规则逻辑，使用更简洁的代码实现。
    
    - 使用 `parseWechat` 函数简化解析逻辑。
    - 将正则表达式和处理函数映射关系改为规则数组。
    - 使用 `RuleObject` 类封装解析结果。

--- src/rule/com.tencent.mm/app/微信公众号中国邮政储蓄银行/main.js
+++ src/rule/com.tencent.mm/app/微信公众号中国邮政储蓄银行/main.js
@@ -1,11 +1,11 @@
-import { BillType, Currency, formatDate, RuleObject, toFloat } from 'common/index.js';
+import { BillType, Currency, formatDate, parseWechat, RuleObject, toFloat } from 'common/index.js';
 
 // 定义源名称和需要匹配的标题数组
 const SOURCE = '中国邮政储蓄银行';
-const TITLES = ['交易提醒'];
+const TITLE = ['交易提醒'];
 
 // 正则表达式和处理函数的映射关系
-const regexMapBOC = [
+const rules = [
   [
     //交易时间：2024年09月18日08:18\n交易类型：(尾号8057)快捷支付\n交易金额：(人民币)24.92元
     //交易时间：2024年06月14日23:19\n交易类型：(尾号7618)薪酬\n交易金额：(人民币)8683.33元
@@ -20,63 +20,28 @@ const regexMapBOC = [
       }
 
 
-      return {
-        "money": toFloat(money),
-        "type": type,
-        "time":time,
-        "shopItem": shopItem,
-        "accountNameFrom": `${SOURCE}(${number})`,
-        "Currency": Currency['人民币'],
-        "channel": `微信[${SOURCE}-${typeName}]`,
-      };
+
+      return new RuleObject(
+        type,
+        toFloat(money),
+        '',
+        shopItem,
+        `${SOURCE}(${number})`,
+        '',
+        0.0,
+        Currency['人民币'],
+        formatDate(time, 'Y年M月D日h:i'),
+        `微信[${SOURCE}-${typeName}]`
+      )
     },
   ],
 ];
 
-/**
- * @param {string} text - 需要解析的文本
- * @returns {Object|null} - 解析结果对象，如果解析失败则返回null
- */
-function parseBOCText(text) {
-  for (let [regex, handler] of regexMapBOC) {
-    const match = text.match(regex);
-    if (match) {
-      return handler(match);
-    }
-  }
-  return null;
-}
 
 /**
  * @param {string} data - JSON格式的数据
  * @returns {RuleObject|null} - 规则对象，如果获取失败则返回null
  */
 export function get(data) {
-  const mapItem = JSON.parse(data).mMap;
-  if (
-    mapItem.source !== SOURCE ||
-    !TITLES.includes(mapItem.title)
-  ) {
-    return null;
-  }
-
-  // 解析文本
-  const parsedText = parseBOCText(mapItem.description);
-  if (!parsedText || parsedText.type === null) {
-    return null;
-  }
-
-  // 创建并返回RuleObject对象
-  return new RuleObject(
-    parsedText.type,
-    parsedText.money,
-    parsedText.shopName,
-    parsedText.shopItem,
-    parsedText.accountNameFrom,
-    parsedText.accountNameTo,
-    0,
-    parsedText.Currency, //024年06月14日23:19
-    formatDate(parsedText.time, 'Y年M月D日h:i'),
-    parsedText.channel
-  );
+  return parseWechat(data, rules, SOURCE, TITLE);
 }
