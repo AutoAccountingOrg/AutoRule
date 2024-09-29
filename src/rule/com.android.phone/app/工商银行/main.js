b93d61d8b089deaf39d1482344bd839f702b6edb
ankio <ankio@ankio.net>
2024-09-29 15:32:08 +0800

    :sparkles: (工商银行): 添加工商银行ETC支出规则 #367
    添加工商银行ETC支出规则，用于识别并解析从工商银行发送的ETC支出短信。
    
    - 规则使用正则表达式匹配短信内容。
    - 支持解析时间、金额、商户名称、交易类型等信息。
    - 使用测试用例验证规则的准确性。

--- src/rule/com.android.phone/app/工商银行/main.js
+++ src/rule/com.android.phone/app/工商银行/main.js
@@ -1,23 +1,55 @@
 import { BillType, Currency, formatDate, RuleObject, splitSms, toFloat } from 'common/index.js';
-export function get(data) {
-  let { sender,bankName,text,t } = splitSms(data);
-  if (bankName !== "工商银行") return null;
-  //尾号1234卡9月27日18:55支出(消费支付宝-北京三快在线科技有限公司)45元，余额1,333.22元。【工商银行】
-  let regex  = /尾号(\d{4})卡(.*?)支出\((.*?)-(.*?)\)([\d,]+(.\d{2})?)元，余额([\d,]+.\d{2})元。/;
-  const match = text.match(regex);
-  if (!match) return null;
-  let [,number,date,shopName,shopItem,money,,] = match;
 
-  let obj = new RuleObject();
+let rules = [
+  {
+    // 尾号1234卡9月27日18:55支出(消费支付宝-北京三快在线科技有限公司)45元，余额1,333.22元。【工商银行】
+    'regex': /尾号(\d{4})卡(.*?)支出\((.*?)-(.*?)\)([\d,]+(.\d{2})?)元，余额([\d,]+.\d{2})元。/,
+    'match': (match) => {
+      let [, number, date, shopName, shopItem, money, ] = match;
+      let obj = new RuleObject();
 
-  obj.money = toFloat(money);
-  obj.channel = `工商银行[支出]`;
-  obj.shopName =shopName;
-  obj.shopItem = shopItem;
-  obj.time = formatDate(date,"M月D日h:i");
+      obj.money = toFloat(money);
+      obj.channel = `工商银行[支出]`;
+      obj.shopName = shopName;
+      obj.shopItem = shopItem;
+      obj.time = formatDate(date, 'M月D日h:i');
 
-  obj.type = BillType.Expend
+      obj.type = BillType.Expend;
 
-  obj.accountNameTo = `工商银行(${number})`;
-  return obj;
+      obj.accountNameFrom = `工商银行(${number})`;
+      return obj;
+    }
+  },
+  {
+    //尊敬的ETC客户：您好！您的车辆(****7SN)2024年09月26日在福建龙岩铁山站驶入至福建南安梅山站驶出共计通行1次高速公路。上述服务合计消费103.55元，您的账户（尾号：7246）已成功扣款103.55元。为避免影响您使用福建高速ETC，请关注您的扣款账户情况。ETC消费记录详单可登陆“福建高速公路”微信公众号查询或拨打12122进行咨询。如需退订此通知短信请回复“TDXS#FJMT”办理【工商银行】
+    'regex': /您的车辆\(.*?\)(.*?)在(.*?)。上述服务合计消费.*?元，您的账户（尾号：(\d{4})）已成功扣款([\d,]+.\d{2})元。为避免影响您使用(.*?)，请关注您的扣款账户情况。/,
+    'match': (match) => {
+      let [, date, shopItem, number, money, shopName] = match;
+      let obj = new RuleObject();
+
+      obj.money = toFloat(money);
+      obj.channel = `工商银行[ETC支出]`;
+      obj.shopName = shopName;
+      obj.shopItem = shopItem;
+      obj.time = formatDate(date, 'Y年M月D日');
+
+      obj.type = BillType.Expend;
+
+      obj.accountNameFrom = `工商银行(${number})`;
+      return obj;
+    }
+  }
+];
+
+export function get (data) {
+  let { sender, bankName, text, t } = splitSms(data);
+  if (bankName !== '工商银行') {
+    return null;
+  }
+  for (let rule of rules) {
+    const match = text.match(rule.regex);
+    if (match) {
+      return rule.match(match);
+    }
+  }
 }
--- src/rule/com.android.phone/app/工商银行/main.test.js
+++ src/rule/com.android.phone/app/工商银行/main.test.js
@@ -10,10 +10,27 @@ test('工商银行支出', () =>
       "fee": 0,
       "shopName": '消费支付宝',
       "shopItem": '北京三快在线科技有限公司',
-      "accountNameFrom": '',
-      "accountNameTo": '工商银行(1234)',
+      "accountNameFrom": '工商银行(1234)',
+      "accountNameTo": '',
       "currency": 'CNY',
       "time": formatDate("9月27日18:55","M月D日h:i"),
       "channel": '工商银行[支出]',
     },
   ]));
+
+test('工商银行ETC支出', () =>
+  testAnkio('工商银行ETC支出', [
+    {
+      "type": "Expend",
+      "money": 103.55,
+      "fee": 0,
+      "shopName": '福建高速ETC',
+      "shopItem": '福建龙岩铁山站驶入至福建南安梅山站驶出共计通行1次高速公路',
+      "accountNameFrom": '工商银行(7246)',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": formatDate("2024年09月26日","Y年M月D日"),
+      "channel": '工商银行[ETC支出]',
+    },
+  ]));
+
