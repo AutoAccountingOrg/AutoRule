2b622c6a596b19c360c694f137c503aec94f8db5
ankio <ankio@ankio.net>
2024-09-30 22:01:00 +0800

    :sparkles: (api): 添加广东农信微信公众号规则 #338
    - 添加了新的规则，用于解析广东农信微信公众号消息，并将信息转换为账单数据
    - 新增了测试用例，用于验证规则的正确性

---
+++ src/rule/com.tencent.mm/app/微信公众号广东农信/main.js
@@ -0,0 +1,41 @@
+import { BillType, Currency, formatDate, parseWechat, RuleObject, toFloat } from 'common/index.js';
+
+// 定义源名称和需要匹配的标题数组
+const SOURCE = '广东农信';
+const TITLE = ['交易提醒'];
+
+// 正则表达式和处理函数的映射关系
+const rules = [
+  [
+    //交易账号:借记卡（尾号0666）\n交易时间:2024-09-25 12:09\n交易类型:收入\n交易金额:人民币1000.00元\n可用余额:人民币8888.12元
+    /交易账号:借记卡（尾号(\d{4})）\n交易时间:(.*?)\n交易类型:(.*?)\n交易金额:(.*?)([\d,]+.\d{2})元\n可用余额:(.*?)元/,
+     match => {
+      let [, number, time, type, currency, money, balance] = match;
+      let matchType = BillType.Expend
+      let shopItem = type;
+      let matchTypeName = "支出";
+      if (type.indexOf('收入') !== -1) {
+        shopItem = type;
+        matchType = BillType.Income;
+        matchTypeName = "收入";
+      }
+
+      return new RuleObject(
+        matchType,
+        toFloat(money),
+        '',
+        shopItem,
+        `广东农信(${number})`,
+        '',
+        0.0,
+        Currency[currency], // 5月7日10:50
+        formatDate(time, 'Y-M-D h:i'),
+        `微信[${SOURCE}-${matchTypeName}]`
+      );
+    }
+  ]
+];
+
+export function get(data) {
+  return parseWechat(data, rules, SOURCE, TITLE);
+}
---
+++ src/rule/com.tencent.mm/app/微信公众号广东农信/main.test.js
@@ -0,0 +1,23 @@
+const { get } = require('./main');
+const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
+
+const { formatDate } = require('common/index.js');
+
+testAnkioInit(get, __dirname, 'com.tencent.mm');
+
+test('广东农信收入', () =>
+  testAnkio('广东农信收入', [
+    {
+      "type": "Income",
+      "money": 1000,
+      "fee": 0,
+      "shopName": '',
+      "shopItem": '收入',
+      "accountNameFrom": '广东农信(0666)',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": formatDate('2024-09-25 12:09', 'Y-M-D h:i'),
+      "channel": '微信[广东农信-收入]',
+    },
+
+  ]));
