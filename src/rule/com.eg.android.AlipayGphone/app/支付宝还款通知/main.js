fbca2b8df65a8b10148117f9a2da3deb45705778
ankio <ankio@ankio.net>
2024-09-30 13:47:19 +0800

    :sparkles: (tests): 添加支付宝还款通知测试用例 #347
    添加了新的支付宝还款通知测试用例，用于验证规则的正确性。

--- src/rule/com.eg.android.AlipayGphone/app/支付宝还款通知/main.js
+++ src/rule/com.eg.android.AlipayGphone/app/支付宝还款通知/main.js
@@ -12,6 +12,10 @@ function transaction(content,t){
 
   obj.channel = '支付宝[还款成功]';
 
+  obj.shopItem = content.date;
+
+  obj.shopName = content.status;
+
   return obj;
 }
 
@@ -19,8 +23,8 @@ export function get(data) {
   let json = JSON.parse(data)[0];
   let pl = JSON.parse(json.pl);
   let t = json.mct;
-  if (pl.title.indexOf('还款成功') === -1) {
-    return null;
+  if (["还款成功","还款到账成功"].includes(pl.title) === false) {
+ return null;
   }
   let content = JSON.parse(pl.content);
   return transaction(content,t);
