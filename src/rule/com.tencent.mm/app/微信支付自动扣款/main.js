aaf5160c17095cf1dfa4d709efa7bfbdab0f6152
ankio <ankio@ankio.net>
2024-09-30 19:55:21 +0800

    :sparkles: (ui): 完善微信支付自动扣款测试用例 #319
    - 添加了地铁支付的测试用例。
    - 补充了更多微信支付自动扣款的测试用例。
    - 更新测试用例，以更好地匹配实际情况。

--- src/rule/com.tencent.mm/app/微信支付自动扣款/main.js
+++ src/rule/com.tencent.mm/app/微信支付自动扣款/main.js
@@ -9,15 +9,16 @@ const TITLE_WECHAT = [
 // 正则表达式和处理函数的映射关系
 const rules =[
   [
-    /扣费金额￥(\d+\.\d{2})\n(扣费服务(.*?)\n)?扣费(内容|项目)(.*?)\n支付方式(.*?)\n收单机构.*(\n备注(.*?。))?/,
+    // 扣费金额￥4.50\n扣费项目成都地铁微信免密支付\n扣费方式工商银行\n收单机构财付通支付科技有限公司\n乘车路线5号线皇花园-6号线牛王庙\n乘车时间2024-09-23 09:15:38\n备注你在成都地铁的账号成都地铁扣费成功，你可以点击消息管理你的扣费项目
+
+    /扣费金额￥(\d+\.\d{2})\n扣费项目(.*?)\n扣费方式(.*?)\n收单机构(.*?)\n乘车路线(.*?)\n乘车时间(.*?)\n备注(.*?)扣费成功，你可以点击消息管理你的扣费项目/,
     (match,t,item) => {
-      const [, money, , shopName, , shopItem, accountNameFrom, , remark] =
-        match;
+      const [, money, shopName, accountNameFrom,,shopItem, time,  remark] = match;
       return new RuleObject(
         BillType.Expend,
         toFloat(money),
-        findNonEmptyString(shopName,shopItem),
-        findNonEmptyString(remark,shopItem),
+        shopName,
+        shopItem,
         accountNameFrom,
         '',
         0.0,
@@ -27,6 +28,50 @@ const rules =[
       );
     },
   ],
+    [
+      // 扣费金额￥53.72\n扣费服务滴滴出行\n扣费内容先乘后付\n支付方式零钱通\n收单机构财付通支付科技有限公司
+      /扣费金额￥(\d+\.\d{2})\n扣费服务(.*?)\n扣费内容(.*?)\n支付方式(.*?)\n收单机构(.*?)$/,
+      (match,t,item) => {
+        const [, money, shopName,shopItem, accountNameFrom,, remark] = match;
+        return new RuleObject(
+          BillType.Expend,
+          toFloat(money),
+          shopName,
+          shopItem,
+          accountNameFrom,
+          '',
+          0.0,
+          Currency['人民币'],
+          t,
+          '微信[微信支付-扣款]'
+        );
+      },
+    ],
+    // 扣费金额￥53.72\n扣费服务滴滴出行\n扣费内容先乘后付\n支付方式零钱通\n收单机构财付通支付科技有限公司
+    // 扣费金额￥51.70\n扣费服务滴滴出行\n扣费内容先乘后付\n支付方式零钱通\n收单机构财付通支付科技有限公司
+    [
+      // 扣费金额￥18.50\n扣费项目腾讯王卡微信话费代扣\n支付方式零钱\n收单机构财付通支付科技有限公司\n备注你的江苏联通账号（19999xxxx）扣费成功。如果对扣费有疑问，请联系商户确认
+      // 扣费金额￥19.00\n扣费项目Keep会员包月自动续费\n支付方式零钱\n收单机构财付通支付科技有限公司\n备注你的Keep账号（太公摘花）扣费成功。如果对扣费有疑问，请联系商户确认
+      //扣费金额￥25.00\n扣费项目迅雷超级会员微信自动续费\n支付方式中国银行储蓄卡(7575)\n收单机构财付通支付科技有限公司\n备注你的深圳市迅雷网络技术有限公司账号（185*****230）扣费成功。如果对扣费有疑问，请联系商户确认
+      /扣费金额￥(\d+\.\d{2})\n扣费项目(.*?)\n支付方式(.*?)\n收单机构(.*?)\n备注(.*?)。如果对扣费有疑问，请联系商户确认/,
+      (match,t,item) => {
+        const [, money, shopName, accountNameFrom,remark,shopItem] = match;
+        return new RuleObject(
+          BillType.Expend,
+          toFloat(money),
+          shopName,
+          shopItem,
+          accountNameFrom,
+          '',
+          0.0,
+          Currency['人民币'],
+          t,
+          '微信[微信支付-扣款]'
+        );
+      },
+    ],
+    //
+
 ];
 
 
