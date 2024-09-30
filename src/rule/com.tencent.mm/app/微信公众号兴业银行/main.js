ce6166b6b3146001a382194fb8898e70483533b1
ankio <ankio@ankio.net>
2024-09-30 21:35:43 +0800

    :sparkles: (api): 添加兴业银行微信公众号规则 #315
    添加了兴业银行微信公众号的支出规则，支持解析交易时间、交易类型、交易金额、卡内余额等信息。

---
+++ src/rule/com.tencent.mm/app/微信公众号兴业银行/main.js
@@ -0,0 +1,42 @@
+import { BillType, Currency, formatDate, parseWechat, RuleObject, toFloat, transferCurrency } from 'common/index.js';
+
+// 定义源名称和需要匹配的标题数组
+const SOURCE = '兴业银行';
+const TITLE = ['交易提醒'];
+
+// 正则表达式和处理函数的映射关系
+const rules = [
+  [
+    // 交易时间：2024年09月22日  20:40\n交易类型：借记卡*4715跨行消费支出\n交易金额：5,192.00 元\n卡内余额：624.18 元
+    //5月7日10:50
+    /交易时间：(.*?)\n交易类型：借记卡\*(\d{4})(.*?)\n交易金额：([\d,]+.\d{2}) 元\n卡内余额：([\d,]+.\d{2}) 元/,
+     match => {
+      let [,time, number, type, money, balance] = match;
+      let matchType = BillType.Expend
+      let shopItem = type;
+      let matchTypeName = "支出";
+      if (type.indexOf('收入') !== -1) {
+        shopItem = type;
+        matchType = BillType.Income;
+        matchTypeName = "收入";
+      }
+
+      return new RuleObject(
+        matchType,
+        toFloat(money),
+        '',
+        shopItem,
+        `兴业银行借记卡(${number})`,
+        '',
+        0.0,
+        transferCurrency("人民币"), // 2024年09月22日  20:40
+        formatDate(time, 'Y年M月D日  h:i'),
+        `微信[${SOURCE}-${matchTypeName}]`
+      );
+    }
+  ]
+];
+
+export function get(data) {
+  return parseWechat(data, rules, SOURCE, TITLE);
+}
---
+++ src/rule/com.tencent.mm/app/微信公众号兴业银行/main.test.js
@@ -0,0 +1,22 @@
+const { get } = require('./main');
+const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
+
+const { formatDate } = require('common/index.js');
+
+testAnkioInit(get, __dirname, 'com.tencent.mm');
+
+test('兴业银行支出', () =>
+  testAnkio('兴业银行支出', [
+    {
+      "type": "Expend",
+      "money": 5192,
+      "fee": 0,
+      "shopName": '',
+      "shopItem": '跨行消费支出',
+      "accountNameFrom": '兴业银行借记卡(4715)',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": formatDate('2024年09月22日  20:40', 'Y年M月D日  h:i'),
+      "channel": '微信[兴业银行-支出]',
+    },
+  ]));
