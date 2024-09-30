56efd646d402f011659944f3fa57063be81d30ed
ankio <ankio@ankio.net>
2024-09-30 13:26:03 +0800

    :sparkles: (rule): 添加支付宝信用卡退款规则
    添加了识别支付宝信用卡退款消息的规则，可以准确提取交易信息，例如金额、商户名称、商品名称等。

---
+++ src/rule/com.eg.android.AlipayGphone/app/支付宝信用卡交易/main.js
@@ -0,0 +1,39 @@
+import { BillType, RuleObject, toFloat } from 'common/index.js';
+
+function transaction(extras,t,accountNameFrom){
+
+  let typeName = extras.assistMsg1;
+
+  let shopName  = extras.assistMsg3;
+
+  let money = toFloat(extras.assistMsg4);
+
+  let shopItem = extras.assistMsg5;
+
+
+  let type = BillType.Expend;
+  if ([ '退货'].includes(typeName)) {
+    type = BillType.Income;
+  }
+  return new RuleObject(
+    type,
+    money,
+    shopName,
+    shopItem,
+    accountNameFrom,
+    '',
+    0.0,'CNY',
+    t,'支付宝[信用卡交易提醒]'
+  );
+}
+
+export function get(data) {
+  let json = JSON.parse(data)[0];
+  let pl = JSON.parse(json.pl);
+  let t = json.mct;
+  if (pl.title.indexOf('信用卡交易提醒') === -1) {
+    return null;
+  }
+  let extras = JSON.parse(pl.extraInfo);
+  return transaction(extras,t,pl.bizName);
+}
