9db6404651d18316e0300da613331afd56dbc57b
ankio <ankio@ankio.net>
2024-10-18 10:02:37 +0800

    :sparkles: (rule): 添加支付宝小猪存钱罐规则
    添加支付宝小猪存钱罐规则，用于解析支付宝小猪存钱罐的转账记录。
    
    该规则可以识别小猪存钱罐自动攒入的交易，并提取相关信息，例如转入金额、交易时间、交易对象等。

---
+++ src/rule/com.eg.android.AlipayGphone/app/支付宝小猪存钱罐/main.js
@@ -0,0 +1,24 @@
+import { AliTools, BillType, RuleObject, toFloat } from 'common/index.js';
+
+export function get(data) {
+  let json = JSON.parse(data)[0];
+  let pl = JSON.parse(json.pl);
+  //余额宝-小猪攒钱罐-自动攒入
+  if (
+    pl.title.indexOf('余额宝-小猪攒钱罐-自动攒入') === -1) {
+    return null;
+  }
+
+  let t = json.mct;
+  let obj = new RuleObject();
+
+  let content = JSON.parse( pl.content);
+  AliTools.handleContentItems(content.content, obj);
+
+  obj.channel = `支付宝[余额宝-小猪攒钱罐]`;
+  obj.time = t;
+  obj.type = BillType.Transfer;
+  obj.accountNameTo = '支付宝余额宝';
+  obj.shopItem = '小猪攒钱罐';
+  return obj;
+}
