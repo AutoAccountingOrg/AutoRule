7d6df3612355f0a6569b23ebc8e88bff82a69ac0
ankio <ankio@ankio.net>
2024-10-18 15:17:10 +0800

    :sparkles: (rule): 添加支付宝笔笔攒转入规则 #399
    新增支付宝笔笔攒转入规则，自动识别并提取相关信息。
    
    - 识别“笔笔攒转入成功”消息
    - 提取金额、时间、交易对象等信息
    - 匹配交易渠道、账户名称、交易类型
    - 生成规范化的账单记录

---
+++ src/rule/com.eg.android.AlipayGphone/app/支付宝笔笔攒/main.js
@@ -0,0 +1,24 @@
+import { AliTools, BillType, RuleObject, toFloat } from 'common/index.js';
+
+export function get(data) {
+  let json = JSON.parse(data)[0];
+  let pl = JSON.parse(json.pl);
+  let t = json.mct;
+  if (pl.templateName.indexOf('笔笔攒') !== -1) {
+    let obj = new RuleObject(BillType.Transfer);
+
+    obj.channel = `支付宝[笔笔攒-转入]`;
+
+    let content = JSON.parse(pl.content);
+    AliTools.handleContentItems(content.content, obj);
+    obj.money = toFloat(content.money);
+    obj.shopName = "余额宝";
+    obj.shopItem = pl.homePageTitle;
+    obj.time = t;
+    obj.accountNameTo = '余额宝';
+
+
+    return obj;
+  }
+  return null;
+}
