6f7eefb63e1ec48fb1318e263895e44ba89b6f64
TrioTea <111053672+TrioTea@users.noreply.github.com>
2024-11-24 15:16:18 +0800

    :recycle: (rule) 优化微信-农业银行信用卡解析规则 (#526)
    - 修改支出正则表达式及对应测试用例
    - 增加刷卡金返现场景的解析规则及对应测试用例

--- src/rule/com.tencent.mm/app/微信公众号农业银行信用卡/main.test.js
+++ src/rule/com.tencent.mm/app/微信公众号农业银行信用卡/main.test.js
@@ -13,12 +13,28 @@ test('农业银行信用卡支出', () =>
       "type": "Expend",
       "money": 1281.56,
       "fee": 0,
-      "shopName": '财付通，微信支付',
-      "shopItem": '京东商城平台商户',
+      "shopName": '财付通',
+      "shopItem": '微信支付-京东商城平台商户',
       "accountNameFrom": '农业银行信用卡(5981)',
       "accountNameTo": '',
       "currency": 'CNY',
       "time": formatDate('2024-05-26 20:09:20', 'Y-M-D h:i:s'),
-      "channel": '微信[农业银行信用卡-消费]',
+      "channel": '财付通[农业银行信用卡-消费]',
     },
   ]));
+
+test("农业银行信用卡刷卡金返现", () =>
+  testAnkio('农业银行信用卡刷卡金返现', [
+    {
+      "type": "Income",
+      "money": 0.3,
+      "fee": 0,
+      "shopName": '农业银行信用卡',
+      "shopItem": '天天返现',
+      "accountNameFrom": '农业银行信用卡(8487)',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": formatDate('2024-11-21 09:15:06', 'Y-M-D h:i:s'),
+      "channel": '刷卡金返现[农业银行信用卡-收入]'
+    }
+    ]));
