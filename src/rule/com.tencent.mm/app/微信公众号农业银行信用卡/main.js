a97426da0a03d25ca4df0fbecf3dce1d48cad36f
TrioTea <triocuptea@gmail.com>
2024-11-24 13:56:31 +0800

    :recycle: (rule) 优化微信-农业银行信用卡解析规则 - 修改支出正则表达式及对应测试用例 - 增加刷卡金返现场景的解析规则及对应测试用例

--- src/rule/com.tencent.mm/app/微信公众号农业银行信用卡/main.js
+++ src/rule/com.tencent.mm/app/微信公众号农业银行信用卡/main.js
@@ -6,8 +6,9 @@ const TITLE = ['交易成功通知'];
 
 // 正则表达式和处理函数的映射关系
 const rules = [
+  // 支出规则
   [
-    /交易时间：(.*?)\n交易类型：卡号尾号（(\d+)），网上支付\n交易金额：([\d,]+.\d{2})元\n可用余额：.*?元\n交易地址：(.*?)-(.*?)$/,
+    /交易时间：(.*?)\n交易类型：卡号尾号（(\d+)），网上支付\n交易金额：([\d,]+.\d{2})元\n可用余额：.*?元\n交易地址：(.*?)，(.*?)$/,
     match => {
       const [, time, number, money, shopName, shopItem] = match;
 
@@ -21,7 +22,27 @@ const rules = [
         0.0,
         Currency['人民币'],
         formatDate(time, 'Y-M-D h:i:s'),
-        `微信[${SOURCE}-消费]`
+        `${shopName}[${SOURCE}-消费]`
+      )
+    },
+  ],
+  // 收入规则（刷卡金返现收入）
+  [
+    /交易时间：(.*?)\n交易类型：卡号尾号（(\d+)），刷卡金转入\n交易金额：([\d,]+.\d{2})元\n可用余额：.*?元\n交易地址：(.*?),消费时间.*$/,
+    match => {
+      const [, time, number, money, description] = match;
+
+      return new RuleObject(
+        BillType.Income,  // 收入类型
+        toFloat(money),
+        SOURCE,
+        description,     // 描述（如：天天返现）
+        `${SOURCE}(${number})`,
+        '',
+        0.0,
+        Currency['人民币'],
+        formatDate(time, 'Y-M-D h:i:s'),
+        `刷卡金返现[${SOURCE}-收入]`
       )
     },
   ],
