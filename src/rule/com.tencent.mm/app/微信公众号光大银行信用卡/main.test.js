5e5a563d77554c021a451c173143b161b0ba005c
TrioTea <triocuptea@gmail.com>
2024-11-28 17:27:41 +0800

    :tada: (app): 添加微信公众号光大银行信用卡规则https://github.com/AutoAccountingOrg/AutoRule/issues/533 - 支持消费支出识别

---
+++ src/rule/com.tencent.mm/app/微信公众号光大银行信用卡/main.test.js
@@ -0,0 +1,21 @@
+const { get } = require('./main');
+const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
+const { formatDate } = require('common/index.js');
+
+testAnkioInit(get, __dirname, 'com.tencent.mm');
+
+test('光大银行信用卡消费', () =>
+  testAnkio('光大银行信用卡消费', [
+    {
+      "type": "Expend",
+      "money": 10000,
+      "fee": 0,
+      "shopName": '财付通',
+      "shopItem": '华强电子世界',
+      "accountNameFrom": '光大银行信用卡(5338)',
+      "accountNameTo": '',
+      "currency": 'CNY',
+      "time": formatDate('2024年11月25日 18:55', 'Y年M月D日 H:i'),
+      "channel": '微信[光大银行信用卡-支出]',
+    }
+  ]));
