675f1ccbdeeeee9b912ba047f255c8c2a7c49773
TrioTea <111053672+TrioTea@users.noreply.github.com>
2024-12-02 16:44:31 +0800

    :tada: 新增微信公众号农业银行退款规则、支出额外兼容一种可能出现的消息格式 (#569)

--- src/rule/com.tencent.mm/app/微信公众号农业银行信用卡/main.test.js
+++ src/rule/com.tencent.mm/app/微信公众号农业银行信用卡/main.test.js
@@ -23,6 +23,22 @@ test('农业银行信用卡支出', () =>
     },
   ]));
 
+test('农业银行信用卡支出2', () =>
+  testAnkio('农业银行信用卡支出2', [
+    {
+      "type": "Expend",
+      "money": 2.00,
+      "fee": 0,
+      "shopName": "杭州兑吧网络科技有限公司",
+      "shopItem": "杭州兑吧网络科技有限公司",
+      "accountNameFrom": "农业银行信用卡(8487)",
+      "accountNameTo": "",
+      "currency": "CNY",
+      "time": formatDate('2024-12-02 08:21:01', 'Y-M-D h:i:s'),
+      "channel": "杭州兑吧网络科技有限公司[农业银行信用卡-消费]"
+    }
+  ]));
+
 test("农业银行信用卡刷卡金返现", () =>
   testAnkio('农业银行信用卡刷卡金返现', [
     {
@@ -37,4 +53,20 @@ test("农业银行信用卡刷卡金返现", () =>
       "time": formatDate('2024-11-21 09:15:06', 'Y-M-D h:i:s'),
       "channel": '刷卡金返现[农业银行信用卡-收入]'
     }
-    ]));
+  ]));
+
+test('农业银行信用卡退款', () =>
+  testAnkio('农业银行信用卡退款', [
+    {
+      "type": "Income",
+      "money": 17.67,
+      "fee": 0,
+      "shopName": "多多支付退款",
+      "shopItem": "多多支付退款",
+      "accountNameFrom": "农业银行信用卡(6395)",
+      "accountNameTo": "",
+      "currency": "CNY",
+      "time": formatDate('2024-12-02 12:32:19', 'Y-M-D h:i:s'),
+      "channel": "退款[农业银行信用卡-收入]"
+    }
+  ]));
