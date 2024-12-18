95429cfa86bfc57b1d41a15a689695a1808dd36a
ankio <ankio@ankio.net>
2024-12-18 11:58:47 +0800

    :sparkles: (rule): 添加北京银行短信支出解析规则及相关测试  #473
    新增北京银行短信支出解析规则，支持解析账户支付信息并生成对应账单对象。包含以下内容：
    - 正则表达式匹配短信内容，提取账户号、日期、支付方式、金额、余额及对方户名等信息。
    - 根据解析结果生成账单对象，设置支出类型、支付渠道、商户名称等属性。
    - 添加单元测试，验证解析逻辑的正确性。

---
+++ src/rule/com.android.phone/app/短信北京银行/main.js
@@ -0,0 +1,40 @@
+import { BillType, formatDate, RuleObject, splitSms, toFloat } from 'common/index.js';
+
+let rules = [
+  {
+    // 您账户0561于11月6日13:03通过财付通支付1.00元。活期余额13773.30元。对方户名:微信红包。
+    'regex':
+    /您账户(\d{4})于(.*?)通过(.*?)支付(.*?)元。活期余额(.*?)元。对方户名:(.*?)。/,
+    'match': (match,t) => {
+      let [, number, date, shopItem, money,,shopName ] = match;
+      let obj = new RuleObject();
+
+      obj.money = toFloat(money);
+
+      let type = BillType.Expend;
+      let typeName = "支出";
+      obj.channel = `北京银行[${typeName}]`;
+      obj.shopItem = shopItem;
+      obj.time = t;
+      obj.shopName = shopName;
+
+      obj.type = type;
+
+      obj.accountNameFrom = `北京银行(${number})`;
+      return obj;
+    }
+  },
+];
+
+export function get (data) {
+  let { sender, bankName, text, t } = splitSms(data);
+  if (bankName !== '北京银行') {
+    return null;
+  }
+  for (let rule of rules) {
+    const match = text.match(rule.regex);
+    if (match) {
+      return rule.match(match,t);
+    }
+  }
+}
