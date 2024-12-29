e446bf915f92cd76d00b67f8a8aa1dd7df12d372
ankio <ankio@ankio.net>
2024-12-29 21:00:44 +0800

    :sparkles: (rule): 优化微信公众号四川农信的交易信息解析逻辑
    - 在 `main.js` 中引入 `splitShop` 函数，用于拆分交易类型中的店铺名称和商品信息
    - 更新正则表达式以支持更灵活的交易类型匹配
    - 在 `main.test.js` 中调整测试数据，确保 `shopName` 和 `shopItem` 正确解析

--- src/rule/com.tencent.mm/app/微信公众号四川农信/main.js
+++ src/rule/com.tencent.mm/app/微信公众号四川农信/main.js
@@ -1,4 +1,12 @@
-import { formatDate, isPaymentType, parseWechat, RuleObject, toFloat, transferCurrency } from 'common/index.js';
+import {
+  formatDate,
+  isPaymentType,
+  parseWechat,
+  RuleObject,
+  splitShop,
+  toFloat,
+  transferCurrency
+} from 'common/index.js';
 
 // 定义源名称和需要匹配的标题数组
 const SOURCE = '四川农信';
@@ -8,15 +16,18 @@ const TITLE = ['交易动账提醒'];
 const rules = [
   [
     // 交易时间：2024-12-16 19:58\n交易类型：工资(收入)\n交易金额：人民币4,285.71元\n账户余额：人民币4,287.70元
-    /交易时间：(.*?)\n交易类型：(.*?)\n交易金额：(.*?)([\d,]+.\d{2})元(\((.*?)\))?\n账户余额：人民币.*?元/,
+    // 交易时间：2024-05-12 17:51\n交易类型：合众易宝委托付款-提现\n交易金额：人民币11.02元(收入)\n账户余额：人民币200.80元
+    /交易时间：(.*?)\n交易类型：(.*?)\n交易金额：(.*?)([\d,]+.\d{2})元(.*?)?\n账户余额：人民币.*?元/,
     match => {
-      const [, time, type, currency, money] = match;
-      let { matchType, typeName } = isPaymentType(type);
+      const [, time, type, currency, money, type2] = match;
+      let type3 = type + (type2 || '');
+      let { matchType, typeName } = isPaymentType(type3);
+      let { shopName, shopItem } = splitShop(type);
       return new RuleObject(
         matchType,
         toFloat(money),
-        '',
-        type,
+        shopName,
+        shopItem,
         `${SOURCE}`,
         '',
         0.0,
