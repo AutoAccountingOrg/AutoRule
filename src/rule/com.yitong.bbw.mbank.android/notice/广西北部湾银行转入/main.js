dc7b2d9fb1146c114bbbf4c5df478a7940b06f88
ankio <ankio@ankio.net>
2024-12-30 13:58:30 +0800

    :sparkles: (rule): 新增广西北部湾银行转入规则及测试用例 #663
    新增广西北部湾银行转入规则，包括解析逻辑、测试用例及测试数据文件。该规则用于处理广西北部湾银行转入交易的通知，提取交易金额、账户信息等关键数据，并生成相应的账单对象。

---
+++ src/rule/com.yitong.bbw.mbank.android/notice/广西北部湾银行转入/main.js
@@ -0,0 +1,24 @@
+import { BillType, RuleObject, toFloat } from 'common/index.js';
+
+export function get (data) {
+  let json = JSON.parse(data);
+  // 您尾号9820的账户发生转入交易，人民币2065.00元
+  let regex = /您尾号(\d{4})的账户发生转入交易，人民币([\d,]+.\d{2})元/;
+  const match = json.text.match(regex);
+  if (!match) {
+    return null;
+  }
+  let [, account, money] = match;
+  let obj = new RuleObject();
+
+  obj.money = toFloat(money);
+  obj.channel = `广西北部湾银行[转入]`;
+  obj.shopName = '广西北部湾银行';
+  obj.shopItem = json.text;
+  obj.time = json.t;
+
+  obj.type = BillType.Income;
+
+  obj.accountNameFrom = `广西北部湾银行`;
+  return obj;
+}
---
+++ src/rule/com.yitong.bbw.mbank.android/notice/广西北部湾银行转入/main.test.js
@@ -0,0 +1,20 @@
+const { get } = require('./main');
+const { testAnkio, testAnkioInit } = require('../../../../tests/TestUtils');
+const { formatDate } = require('common/Time.js');
+
+testAnkioInit(get, __dirname, 'com.gxbbank');
+test('广西北部湾银行转入', () =>
+  testAnkio('广西北部湾银行转入', [
+    {
+      'type': 'Income',
+      'money': 2065.00,
+      'fee': 0,
+      'shopName': '广西北部湾银行',
+      'shopItem': '您尾号9820的账户发生转入交易，人民币2065.00元',
+      'accountNameFrom': '广西北部湾银行',
+      'accountNameTo': '',
+      'currency': 'CNY',
+      'time': 1735537788207,
+      'channel': '广西北部湾银行[转入]'
+    }
+  ]));
