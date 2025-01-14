a3e41ef4d86015bd5b52b8fa931880d7b29e95f3
ankio <ankio@ankio.net>
2025-01-14 10:18:04 +0800

    :truck: (工商银行): 重命名并扩展工商银行收支规则
    - 将 `工商银行收入` 目录重命名为 `工商银行收支`，以支持支出和收入两种类型的通知。
    - 更新正则表达式以匹配支出和收入的通知格式。
    - 添加支出相关的测试用例和测试数据文件。
    - 确保原有收入功能不受影响，并兼容新的支出功能。

--- src/rule/com.icbc/notice/工商银行收支/main.test.js
+++ src/rule/com.icbc/notice/工商银行收支/main.test.js
@@ -18,3 +18,20 @@ test('工商银行收入', () =>
       'channel': '工商银行[收入]'
     }
   ]));
+
+test('工商银行支出', () =>
+  testAnkio('工商银行支出', [
+    {
+      'type': 'Expend',
+      'money': 4,
+      'fee': 0,
+      'shopName': '消费支付宝',
+      'shopItem': '谢水生',
+      'accountNameFrom': '工商银行(1054)',
+      'accountNameTo': '',
+      'currency': 'CNY',
+      'time': formatDate('1月12日15:56', 'M月D日h:i'),
+      'channel': '工商银行[支出]'
+    }
+  ]));
+
