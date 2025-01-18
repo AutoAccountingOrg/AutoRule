260d563c1a0157e0fa6aeaf27e04df5072f67eeb
ankio <ankio@ankio.net>
2025-01-18 14:33:36 +0800

    :sparkles: (rule): 添加浦发银行信用卡还款功能
    - 在 `main.js` 中添加还款提醒的正则匹配规则
    - 在 `main.test.js` 中添加还款功能的测试用例
    - 新增测试数据文件 `浦发银行信用卡还款.txt`

--- src/rule/com.tencent.mm/app/微信公众号浦发银行信用卡/main.js
+++ src/rule/com.tencent.mm/app/微信公众号浦发银行信用卡/main.js
@@ -2,7 +2,7 @@ import { BillType, formatDate, parseWechat, RuleObject, toFloat, transferCurrenc
 
 // 定义源名称和需要匹配的标题数组
 const SOURCE = '浦发银行信用卡';
-const TITLE = ['交易提醒'];
+const TITLE = ['交易提醒', '还款提醒'];
 
 // 正则表达式和处理函数的映射关系
 const rules = [
@@ -10,7 +10,6 @@ const rules = [
     //银行卡号：尾号7928\n交易时间：07月08日 12:15\n交易类型：消费\n交易金额：人民币3.00元\n商户名称：支付宝-美宜佳（湖北）便利店...
     /银行卡号：尾号(.*?)\n交易时间：(.*?)\n交易类型：(.*?)\n交易金额：(.*?)([\d,]+.\d{2})元\n商户名称：(.*?)$/,
     match => {
-
       let [, number, time, type, currency, money, shopItem] = match;
       return new RuleObject(
         BillType.Expend,
@@ -23,6 +22,24 @@ const rules = [
         transferCurrency(currency),
         formatDate(time, 'M月D日 h:i'),
         `微信[${SOURCE}-消费]`);
+    }
+  ],
+  [
+    //还款时间:2025年01月18日 14:13\n还款金额:￥9.00\n还款卡号:498451******0914\n账单还款进度:已还清
+    /还款时间:(.*?)\n还款金额:￥([\d,]+.\d{2})\n还款卡号:(.*?)\n账单还款进度:(.*?)$/,
+    match => {
+      let [, time, money, number, status] = match;
+      return new RuleObject(
+        BillType.Transfer,
+        toFloat(money),
+        '',
+        '',
+        '',
+        `浦发银行信用卡(${number})`,
+        0.0,
+        'CNY',
+        formatDate(time, 'Y年M月D日 h:i'),
+        `微信[${SOURCE}-还款]`);
     }
   ]
 ];
--- src/rule/com.tencent.mm/app/微信公众号浦发银行信用卡/main.test.js
+++ src/rule/com.tencent.mm/app/微信公众号浦发银行信用卡/main.test.js
@@ -1,3 +1,4 @@
+
 const { get } = require('./main');
 const { testAnkioInit, testAnkio } = require('../../../../tests/TestUtils');
 
@@ -18,5 +19,21 @@ test('浦发银行消费', () =>
       'currency': 'CNY',
       'time': formatDate('07月08日 12:15', 'M月D日 h:i'),
       'channel': '微信[浦发银行信用卡-消费]'
+    }
+  ]));
+
+test('浦发银行信用卡还款', () =>
+  testAnkio('浦发银行信用卡还款', [
+    {
+      'type': 'Transfer',
+      'money': 9,
+      'fee': 0,
+      'shopName': '',
+      'shopItem': '',
+      'accountNameFrom': '',
+      'accountNameTo': '浦发银行信用卡(498451******0914)',
+      'currency': 'CNY',
+      'time': formatDate('2025年01月18日 14:13', 'Y年M月D日 h:i'),
+      'channel': '微信[浦发银行信用卡-还款]'
     }
   ]));
